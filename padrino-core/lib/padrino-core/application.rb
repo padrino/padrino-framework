705df5b521273b83c22cb4d13dcca186f1aa46c5
Davide D'Agostino <DAddYE@MacBook.local>
2009-11-23 11:56:38 +0100

    [padrino-core] Application::reset_routes! return true/false (useful for test)

--- padrino-core/lib/padrino-core/application.rb
+++ padrino-core/lib/padrino-core/application.rb
@@ -135,9 +135,10 @@ module Padrino
       # Resets application routes for use in reloading the application
       # This performs a basic routes reload (compatible with sinatra edge)
       def reset_routes!
-        return if single_app? # Don't reset routes for single app
+        return false if single_app? # Don't reset routes for single app
         @routes = Padrino::Application.dupe_routes
         load(self.app_file)
+        true
       end
 
       # Registers an initializer with the application
