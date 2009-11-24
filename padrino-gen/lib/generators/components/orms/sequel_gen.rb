b6c36da78142d7cc29ab2b3563b6f89363d36230
Nathan Esquenazi <nesquena@gmail.com>
2009-11-23 23:12:56 -0800

    [padrino-gen] Fixing orm component default database connections to include default adapter of sqlite

--- padrino-gen/lib/generators/components/orms/sequel_gen.rb
+++ padrino-gen/lib/generators/components/orms/sequel_gen.rb
@@ -9,9 +9,9 @@ module Padrino
           module DatabaseSetup
             def self.registered(app)
               Sequel::Model.plugin(:schema)
-              app.configure(:development) { Sequel.connect('your_dev_db_here') }
-              app.configure(:production)  { Sequel.connect('your_production_db_here') }
-              app.configure(:test)        { Sequel.connect('your_test_db_here') }
+              app.configure(:development) { Sequel.connect('sqlite://your_dev_db_here') }
+              app.configure(:production)  { Sequel.connect('sqlite://your_production_db_here') }
+              app.configure(:test)        { Sequel.connect('sqlite://your_test_db_here') }
             end
           end
           SEQUEL
