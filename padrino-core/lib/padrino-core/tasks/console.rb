498e2d0077296670e03ce6ed454f02b30a405600
Nathan Esquenazi <nesquena@gmail.com>
2009-11-24 05:45:19 -0800

    [padrino-core] Found better way (i hope) to load models and everything else in console

--- padrino-core/lib/padrino-core/tasks/console.rb
+++ padrino-core/lib/padrino-core/tasks/console.rb
@@ -17,5 +17,5 @@ end
 Padrino.mounted_apps.each do |app|
   puts "=> Loading Application #{app.name}"
   Padrino.require_dependency(app.app_file)
-  ["models/*.rb", "app/models/*.rb"].each { |p| Padrino.require_dependencies(File.join(app.app_object.root, p)) }
+  app.app_object.setup_application!
 end
