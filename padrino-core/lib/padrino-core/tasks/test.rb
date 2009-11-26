0b7304b1e458eb556077a37d8a578b2613bf602d
Davide D'Agostino <DAddYE@MacBook.local>
2009-11-26 18:41:38 +0100

    [padrino-core] Added handler for test bacon

--- padrino-core/lib/padrino-core/tasks/test.rb
+++ padrino-core/lib/padrino-core/tasks/test.rb
@@ -8,8 +8,9 @@ module Padrino
         # It look for any spec/*_spec.rb and test/test_*.rb files in your app root.
         def start
           puts "=> Executing Tests..."
-          tests =  Dir['test/**/test_*.rb'] - ['test/test_helper.rb']
-          tests << Dir['spec/**/*_spec.rb'] - Dir['spec/**/spec_helper.rb']
+          tests  = Dir['test/**/test_*.rb'] - ['test/test_helper.rb']         # TestUnit
+          tests << Dir['test/**/*_test.rb'] - ['test/test_config.rb']         # Bacon
+          tests << Dir['spec/**/*_spec.rb'] - Dir['spec/**/spec_helper.rb']   # Rspec
           cmd = "ruby -rubygems -I.:lib -e'%w( #{tests.join(' ')} ).each { |file| require file }'"
           system cmd
         end
