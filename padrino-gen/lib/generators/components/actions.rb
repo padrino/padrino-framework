e490bcb874980923f80391e3417a568bba28cc99
achiu <mr.arthur.chiu@gmail.com>
2009-11-27 20:10:42 -0800

    cleaned up url generation

--- padrino-gen/lib/generators/components/actions.rb
+++ padrino-gen/lib/generators/components/actions.rb
@@ -108,14 +108,14 @@ module Padrino
             end.join("\n  ")
         end
         
-        #For controller route generation
+        # For controller route generation
         # Takes in the fields and maps out an appropriate default route.
         # where controller is user and route is get:test, will add map(:test).to("/user/test")
         def controller_routes(name,fields)
           field_tuples = fields.collect { |value| value.split(":") }
-          routes = field_tuples.collect do |request, route| 
-            "map(:#{route}).to(\"/#{name}/#{route}\")"
-            end.join("\n  ")
+          routes = "\n" + field_tuples.collect do |request, route| 
+            "  map(:#{route}).to(\"/#{name}/#{route}\")"
+            end.join("\n") + "\n"
         end
         
       end
