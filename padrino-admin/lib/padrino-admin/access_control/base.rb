8e4a0e73333f3ba7ac7fa8f99d1cab1ca45e175f
Davide D'Agostino <DAddYE@MacBook.local>
2009-12-28 13:44:58 +0100

    [padrino-admin] Removed extra spaces.

--- padrino-admin/lib/padrino-admin/access_control/base.rb
+++ padrino-admin/lib/padrino-admin/access_control/base.rb
@@ -1,8 +1,8 @@
 module Padrino
   module AccessControl
-    
+
     class AccessControlError < StandardError; end
-    
+
     # This Class map and get roles/projects for accounts
     # 
     #   Examples:
@@ -38,9 +38,9 @@ module Padrino
     class Base
       @@cache = {}
       cattr_accessor :cache
-      
+
       class << self
-        
+
         # We map project modules for a given role or roles
         def roles_for(*roles, &block)
           roles.any? { |role| raise AccessControlError, "Role #{role} must be a symbol!" unless role.is_a?(Symbol)  }
@@ -49,7 +49,7 @@ module Padrino
           @roles.concat(roles)
           @mappers << lambda { |object| Mapper.new(object, *roles, &block) }
         end
-        
+
         # Returns all roles
         def roles
           @roles.nil? ? [] : @roles.collect(&:to_s)
@@ -62,10 +62,10 @@ module Padrino
         end
       end
     end
-    
+
     class Maps
       attr_reader :allowed, :denied, :role, :project_modules
-      
+
       def initialize(mappers, role, object=nil)
         @role            = role
         maps             = mappers.collect { |m|  m.call(object) }.reject { |m| !m.allowed?(role) }
@@ -74,10 +74,10 @@ module Padrino
         @project_modules = maps.collect(&:project_modules).flatten.uniq
       end
     end
-    
+
     class Mapper
       attr_reader :project_modules, :roles, :denied
-      
+
       def initialize(object, *roles, &block)#:nodoc:
         @project_modules = []
         @allowed         = []
@@ -85,37 +85,37 @@ module Padrino
         @roles           = roles
         object ? yield(self, object.dup) : yield(self)
       end
-      
+
       # Create a new project module
       def project_module(name, path=nil, &block)
         @project_modules << ProjectModule.new(name, path, &block)
       end
-      
+
       # Globally allow an paths for the current role
       def allow(path)
         @allowed << path unless @allowed.include?(path)
       end
-      
+
       # Globally deny an pathsfor the current role
       def deny(path)
         @denied << path unless @allowed.include?(path)
       end
-      
+
       # Return true if role is included in given roles
       def allowed?(role)
         @roles.any? { |r| r == role.to_s.downcase.to_sym }
       end
-      
+
       # Return allowed paths
       def allowed
         @project_modules.each { |pm| @allowed.concat(pm.allowed)  }
         @allowed.uniq
       end
     end
-    
+
     class ProjectModule
       attr_reader :name, :menus, :path
-      
+
       def initialize(name, path=nil, options={}, &block)#:nodoc:
         @name     = name
         @options  = options
@@ -125,28 +125,28 @@ module Padrino
         @allowed << path if path
         yield self
       end
-      
+
       # Build a new menu and automaitcally add the action on the allowed actions.
       def menu(name, path=nil, options={}, &block)
         @menus << Menu.new(name, path, options, &block)
       end
-      
+
       # Return allowed controllers
       def allowed
         @menus.each { |m| @allowed.concat(m.allowed) }
         @allowed.uniq
       end
-      
+
       # Return the original name or try to translate or humanize the symbol
       def human_name
         @name.is_a?(Symbol) ? I18n.t("admin.menus.#{@name}", :default => @name.to_s.humanize) : @name
       end
-      
+
       # Return symbol for the given project module
       def uid
         @name.to_s.downcase.gsub(/[^a-z0-9]+/, '').gsub(/-+$/, '').gsub(/^-+$/, '').to_sym
       end
-      
+
       # Return ExtJs Config for this project module
       def config
         options = @options.merge(:text => human_name)
@@ -155,10 +155,10 @@ module Padrino
         options
       end
     end
-    
+
     class Menu
       attr_reader :name, :options, :items, :path
-      
+
       def initialize(name, path=nil, options={}, &block)#:nodoc:
         @name    = name
         @path    = path
@@ -168,28 +168,28 @@ module Padrino
         @allowed << path if path
         yield self if block_given?
       end
-      
+
       # Add a new submenu to the menu
       def add(name, path=nil, options={}, &block)
         @items << Menu.new(name, path, options, &block)
       end
-      
+
       # Return allowed controllers
       def allowed
         @items.each { |i| @allowed.concat(i.allowed) }
         @allowed.uniq
       end
-      
+
       # Return the original name or try to translate or humanize the symbol
       def human_name
         @name.is_a?(Symbol) ? I18n.t("admin.menus.#{@name}", :default => @name.to_s.humanize) : @name
       end
-      
+
       # Return a unique id for the given project module
       def uid
         @name.to_s.downcase.gsub(/[^a-z0-9]+/, '').gsub(/-+$/, '').gsub(/^-+$/, '').to_sym
       end
-      
+
       # Return ExtJs Config for this menu
       def config
         if @path.blank? && @items.empty?
