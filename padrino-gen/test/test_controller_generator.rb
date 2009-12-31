eaf4abb1f3514f56a6a3e4a9227084e7652e4926
achiu <mr.arthur.chiu@gmail.com>
2009-12-30 20:35:02 -0800

    test controller url_routes deletion

--- padrino-gen/test/test_controller_generator.rb
+++ padrino-gen/test/test_controller_generator.rb
@@ -57,48 +57,64 @@ class TestControllerGenerator < Test::Unit::TestCase
       expected_pattern = /class DemoItemsControllerTest < Test::Unit::TestCase/m
       assert_match_in_file(expected_pattern, @controller_test_path)
     end
-    
+
     # Controller action generation
-        
+
     should "generate actions for get:test post:yada" do
       silence_logger { @app.start(['sample_app', '/tmp', '--script=none', '-t=shoulda'])}
       silence_logger { @contgen.start(['demo_items', "get:test","post:yada",'-r=/tmp/sample_app']) }
       assert_match_in_file(/get :test do\n  end\n/m,@controller_path)
       assert_match_in_file(/post :yada do\n  end\n/m,@controller_path)
     end
-        
+
     should "generate url routes for get:yoda post:yada" do
       silence_logger { @app.start(['sample_app', '/tmp', '--script=none', '-t=shoulda'])}
       silence_logger { @contgen.start(['demo_items', "get:yoda","post:yada",'-r=/tmp/sample_app']) }
       assert_match_in_file(/map\(\:yoda\).to\(\"\/demo_items\/yoda\"\)/m,@route_path)
-      assert_match_in_file(/map\(\:yada\).to\(\"\/demo_items\/yada\"\)/m,@route_path)      
+      assert_match_in_file(/map\(\:yada\).to\(\"\/demo_items\/yada\"\)/m,@route_path)
     end
-    
+
   end
-  
+
   context "the controller destroy option" do
-    
+
     should "destroy controller file" do
       silence_logger { @app.start(['sample_app', '/tmp', '--script=none', '-t=shoulda'])}
       silence_logger { @contgen.start(['demo_items','-r=/tmp/sample_app']) }
       silence_logger { @contgen.start(['demo_items','-r=/tmp/sample_app','-d=true'])}
       assert_no_file_exists(@controller_path)
     end
-    
+
     should "destroy controller test" do
       silence_logger { @app.start(['sample_app', '/tmp', '--script=none', '-t=shoulda'])}
       silence_logger { @contgen.start(['demo_items','-r=/tmp/sample_app']) }
       silence_logger { @contgen.start(['demo_items','-r=/tmp/sample_app','-d=true'])}
       assert_no_file_exists(@controller_test_path)
     end
-    
+
     should "destroy controller test" do
       silence_logger { @app.start(['sample_app', '/tmp', '--script=none', '-t=rspec'])}
       silence_logger { @contgen.start(['demo_items','-r=/tmp/sample_app']) }
       silence_logger { @contgen.start(['demo_items','-r=/tmp/sample_app','-d=true'])}
       assert_no_file_exists('/tmp/sample_app/test/controllers/demo_items_controller_spec.rb')
-    end        
+    end
+    
+    should "destroy controller helper" do
+      silence_logger { @app.start(['sample_app', '/tmp', '--script=none', '-t=rspec'])}
+      silence_logger { @contgen.start(['demo_items','-r=/tmp/sample_app']) }
+      silence_logger { @contgen.start(['demo_items','-r=/tmp/sample_app','-d=true'])}
+      assert_no_file_exists('/tmp/sample_app/app/helpers/demo_items_helper.rb')
+    end
+
+    should "remove url routes" do
+      silence_logger { @app.start(['sample_app', '/tmp', '--script=none', '-t=shoulda'])}
+      silence_logger { @contgen.start(['demo_items', "get:yoda","post:yada",'-r=/tmp/sample_app']) }
+      silence_logger { @contgen.start(['demo_items','-r=/tmp/sample_app','-d=true'])}
+      assert_no_match_in_file(/map\(\:yoda\).to\(\"\/demo_items\/yoda\"\)/m,@route_path)
+      assert_no_match_in_file(/map\(\:yada\).to\(\"\/demo_items\/yada\"\)/m,@route_path)
+    end
+
   end
-  
-  
+
+
 end
