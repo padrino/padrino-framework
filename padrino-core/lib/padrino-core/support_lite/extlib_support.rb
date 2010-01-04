c7076907b179d56534129d39de05b512e96fcd2e
Davide D'Agostino <DAddYE@MacBook.local>
2010-01-04 16:21:31 +0100

    [padrino-core] Fixed support for Extlib#Hash#Slice!

--- padrino-core/lib/padrino-core/support_lite/extlib_support.rb
+++ padrino-core/lib/padrino-core/support_lite/extlib_support.rb
@@ -28,11 +28,35 @@ end
 unless Hash.method_defined?(:slice)
   require 'extlib/hash'
   class Hash
+    # Slice a hash to include only the given keys. This is useful for
+    # limiting an options hash to valid keys before passing to a method:
+    #
+    #   def search(criteria = {})
+    #     assert_valid_keys(:mass, :velocity, :time)
+    #   end
+    #
+    #   search(options.slice(:mass, :velocity, :time))
+    #
+    # If you have an array of keys you want to limit to, you should splat them:
+    #
+    #   valid_keys = [:mass, :velocity, :time]
+    #   search(options.slice(*valid_keys))
     def slice(*keys)
       keys = keys.map! { |key| convert_key(key) } if respond_to?(:convert_key)
       hash = self.class.new
       keys.each { |k| hash[k] = self[k] if has_key?(k) }
       hash
+    end
+
+    # Replaces the hash with only the given keys.
+    # Returns a hash contained the removed key/value pairs
+    #   {:a => 1, :b => 2, :c => 3, :d => 4}.slice!(:a, :b) # => {:c => 3, :d =>4}
+    def slice!(*keys)
+      keys = keys.map! { |key| convert_key(key) } if respond_to?(:convert_key)
+      omit = slice(*self.keys - keys)
+      hash = slice(*keys)
+      replace(hash)
+      omit
     end
   end
 end
@@ -43,7 +67,7 @@ unless Hash.method_defined?(:to_params)
 end
 
 ## Hash#reverse_merge, Hash#reverse_merge!
-unless Hash.method_defined?(:present?)
+unless Hash.method_defined?(:reverse_merge)
   class Hash
     def reverse_merge(other_hash)
       other_hash.merge(self)
