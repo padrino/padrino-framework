fa83f15f882b9d0a0cd5095cb4ba7af48e483ec6
Davide D'Agostino <DAddYE@MacBook.local>
2010-01-13 17:40:21 +0100

    [padrino-core] Show DM rake task only if we use DataMapper.

--- padrino-core/lib/padrino-core/tasks/rake_tasks.rb
+++ padrino-core/lib/padrino-core/tasks/rake_tasks.rb
@@ -19,16 +19,18 @@ task :environment do
   end
 end
 
-namespace :dm do
-  desc "DataMapper: perform migration (reset your db data)"
-  task :migrate => :environment do
-    DataMapper.auto_migrate!
-    puts "=> db:migrate executed"
-  end
+if defined?(DataMapper)
+  namespace :dm do
+    desc "DataMapper: perform migration (reset your db data)"
+    task :migrate => :environment do
+      DataMapper.auto_migrate!
+      puts "=> db:migrate executed"
+    end
 
-  desc "DataMapper: perform upgrade (with a no-destructive way)"
-  task :upgrade => :environment do
-    DataMapper.auto_upgrade!
-    puts "=> db:upgrade executed"
+    desc "DataMapper: perform upgrade (with a no-destructive way)"
+    task :upgrade => :environment do
+      DataMapper.auto_upgrade!
+      puts "=> db:upgrade executed"
+    end
   end
 end
