8c163237adb0073133df01ceb914060faf85d39a
Davide D'Agostino <DAddYE@MacBook.local>
2010-01-13 17:24:16 +0100

    [padrino-core] Added a decent output when a we do a padrino rake db:migrate/upgrade

--- padrino-core/lib/padrino-core/tasks/rake_tasks.rb
+++ padrino-core/lib/padrino-core/tasks/rake_tasks.rb
@@ -23,10 +23,12 @@ namespace :dm do
   desc "DataMapper: perform migration (reset your db data)"
   task :migrate => :environment do
     DataMapper.auto_migrate!
+    puts "=> db:migrate executed"
   end
 
   desc "DataMapper: perform upgrade (with a no-destructive way)"
   task :upgrade => :environment do
     DataMapper.auto_upgrade!
+    puts "=> db:upgrade executed"
   end
 end
