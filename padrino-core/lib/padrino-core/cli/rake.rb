f1c9e18d1d6b9e289e3cd1ad9fe47635524afe95
Davide D'Agostino <DAddYE@MacBook.local>
2010-01-14 00:49:08 +0100

    [padrino-core] Added a new padrino rake, seed that can populate db or what do you want. [padrino-core] Added a shell cli in your padrino rake/seeds

--- padrino-core/lib/padrino-core/cli/rake.rb
+++ padrino-core/lib/padrino-core/cli/rake.rb
@@ -4,7 +4,11 @@ require 'rake/rdoctask'
 require File.dirname(__FILE__) + '/../tasks'
 
 Dir["lib/tasks/**/*.rake"].each { |ext| load(ext) }
-Padrino::Tasks.files.flatten.uniq.each { |ext| load(ext); puts ext }
+Padrino::Tasks.files.flatten.uniq.each { |ext| load(ext) }
+
+def shell
+  @_shell ||= Thor::Shell::Basic.new
+end
 
 task :environment do
   Padrino.logger_env = :test
@@ -12,4 +16,10 @@ task :environment do
     Padrino.require_dependency(app.app_file)
     app.app_object.setup_application!
   end
+end
+
+desc 'Load the seed data from db/seeds.rb'
+task :seed => :environment do
+  seed_file = Padrino.root('db', 'seeds.rb')
+  load(seed_file) if File.exist?(seed_file)
 end
