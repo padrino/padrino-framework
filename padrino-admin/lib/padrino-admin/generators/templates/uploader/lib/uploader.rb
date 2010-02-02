459afe5994f857bb39899179f32c143f7a7db801
Davide D'Agostino <DAddYE@MacBook.local>
2010-02-02 21:27:57 +0100

    [padrino-admin] More readable uploader

--- padrino-admin/lib/padrino-admin/generators/templates/uploader/lib/uploader.rb
+++ padrino-admin/lib/padrino-admin/generators/templates/uploader/lib/uploader.rb
@@ -1,49 +1,66 @@
 class Uploader < CarrierWave::Uploader::Base
 
-  # Include RMagick or ImageScience support
-  #   include CarrierWave::RMagick
-  #   include CarrierWave::ImageScience
-  #   include CarrierWave::MiniMagick
+  ##
+  # Image manipulator library:
+  #
+  # include CarrierWave::RMagick
+  # include CarrierWave::ImageScience
+  # include CarrierWave::MiniMagick
 
-  # Choose what kind of storage to use for this uploader
+  ##
+  # Storage type
+  # 
   storage :file
-  #     storage :s3
+  # storage :s3
 
-  # Override the directory where uploaded files will be stored (default is /public/uploads)
-  # This is a sensible default for uploaders that are meant to be mounted:
+  ##
+  # Directory where uploaded files will be stored (default is /public/uploads)
+  # 
   def store_dir
     "uploads"
   end
 
+  ##
+  # Directory where uploaded temp files will be stored (default is /public/temp)
+  # 
   def cache_dir
     "tmp"
   end
 
-  # Provide a default URL as a default if there hasn't been a file uploaded
-  #     def default_url
-  #       "/images/fallback/" + [version_name, "default.png"].compact.join('_')
-  #     end
+  ##
+  # Default URL as a default if there hasn't been a file uploaded
+  # 
+  # def default_url
+  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
+  # end
+
+  ##
   # Process files as they are uploaded.
+  # 
   # process :resize_to_fit => [640, 480]
   #
-  #     def scale(width, height)
-  #       # do something
-  #     end
+  # def scale(width, height)
+  #   # do something
+  # end
 
+  ##
   # Create different versions of your uploaded files
+  # 
   # version :thumb do
   #   process :resize_to_fit => [128, 128]
   # end
 
-  # Add a white list of extensions which are allowed to be uploaded,
-  # for images you might use something like this:
+  ##
+  # White list of extensions which are allowed to be uploaded:
+  # 
   # def extension_white_list
   #   %w(jpg jpeg gif png)
   # end
 
+  ##
   # Override the filename of the uploaded files
-  #     def filename
-  #       "something.jpg" if original_filename
-  #     end
-
+  # 
+  # def filename
+  #   "something.jpg" if original_filename
+  # end
 end
