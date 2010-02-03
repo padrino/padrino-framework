5b4f046669c0d3def891b28e3e2b96e8d16a043b
Davide D'Agostino <DAddYE@MacBook.local>
2010-02-03 15:46:47 +0100

    [padrino-gen] Now example initializer contain an exception notifier.

--- padrino-gen/lib/padrino-gen/generators/app/config/initializers/example.rb
+++ padrino-gen/lib/padrino-gen/generators/app/config/initializers/example.rb
@@ -4,7 +4,24 @@
 # 
 module ExampleInitializer
   def self.registered(app)
-    # Manipulate 'app' here to register components or adjust configuration
-    # app.set :example, "foo"
+    ##
+    # Simple Redmine Issue
+    # 
+    #   app.error 500 do
+    #     # Delivery error to our server
+    #     boom = env['sinatra.error']
+    #     body = ["#{boom.class} - #{boom.message}:", *boom.backtrace].join("\n  ")
+    #     redmine = ["project: foo", "tracker: Bug", "priority: high"].join("\n")
+    #     logger.error body
+    #     Padrino::Mailer::MailObject.new(
+    #       :subject => "[PROJECT] #{boom.class} - #{boom.message}",
+    #       :to => "exceptions@foo.com", 
+    #       :from => "help@foo.com", 
+    #       :body => [body, redmine].join("\n\n")
+    #     ).deliver
+    #     response.status = 500
+    #     content_type 'text/html', :charset => "utf-8"
+    #     render "errors"
+    #   end
   end
 end
