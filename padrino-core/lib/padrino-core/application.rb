417be152375b9255dd6985a683b017bc40728d5a
Davide D'Agostino <DAddYE@MacBook.local>
2010-02-05 00:08:10 +0100

    [padrino-core/gen/admin] Use app.locale_path instead of app.translations

--- padrino-core/lib/padrino-core/application.rb
+++ padrino-core/lib/padrino-core/application.rb
@@ -146,7 +146,7 @@ module Padrino
         self.require_load_paths
         self.disable :logging # We need do that as default because Sinatra use commonlogger.
         I18n.locale = self.locale
-        I18n.load_path += self.translations
+        I18n.load_path += self.locale_path
         @_configured = true
       end
 
@@ -171,7 +171,7 @@ module Padrino
           set :authentication, false
           # Padrino locale
           set :locale, :en
-          set :translations, Proc.new { Dir[File.join(self.root, "/locale/**/*.{rb,yml}")] }
+          set :locale_path, Proc.new { Dir[File.join(self.root, "/locale/**/*.{rb,yml}")] }
           set :auto_locale, false
           # Plugin specific
           set :padrino_mailer, defined?(Padrino::Mailer)
