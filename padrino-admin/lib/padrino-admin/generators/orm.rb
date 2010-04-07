5f46d4d3f8eaa2bd86910eb54ec38c1d84916388
Adrian Madrid <aemadrid@gmail.com>
2010-04-06 18:59:30 -0700

    Adding sequel orm support for admin and migration tasks (small fix to orm finder for sequel - find(params).first instead of all)

--- padrino-admin/lib/padrino-admin/generators/orm.rb
+++ padrino-admin/lib/padrino-admin/generators/orm.rb
@@ -56,7 +56,7 @@ module Padrino
           case orm
             when :activerecord, :mongomapper, :mongoid then "#{klass_name}.find(#{params})"
             when :datamapper, :couchrest   then "#{klass_name}.get(#{params})"
-            when :sequel then "#{klass_name}.filter(#{params}).all"
+            when :sequel then "#{klass_name}.filter(#{params}).first"
             else raise OrmError, "Adapter #{orm} is not yet supported!"
           end
         end
