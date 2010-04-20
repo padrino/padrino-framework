2c325a57199ae40e3af333cdb0a3890a64087142
Davide D'Agostino <d.dagostino@lipsiasoft.com>
2010-04-20 15:08:53 +0200

    [padrino-core] Fixed padrino g and padrino gen aliases [padrino-core] Fixed "layout" of our logs

--- padrino-core/lib/padrino-core/support_lite.rb
+++ padrino-core/lib/padrino-core/support_lite.rb
@@ -24,6 +24,7 @@
 #
 
 require 'i18n'
+require 'rbconfig'
 require 'active_support/version'
 require 'active_support/core_ext/kernel'
 require 'active_support/core_ext/module'
@@ -62,3 +63,29 @@ end
 # Loads our locales configuration files
 #
 I18n.load_path += Dir["#{File.dirname(__FILE__)}/locale/*.yml"]
+
+module Padrino
+  ##
+  # This method return the correct location of padrino bin or
+  # exec it using Kernel#system with the given args
+  #
+  def self.bin(*args)
+    @_padrino_bin ||= [self.ruby_command, File.expand_path("../../../bin/padrino", __FILE__)]
+    args.empty? ? @_padrino_bin : system(args.unshift(@_padrino_bin).join(" "))
+  end
+
+  ##
+  # Return the path to the ruby interpreter taking into account multiple
+  # installations and windows extensions.
+  #
+  def self.ruby_command
+    @ruby_command ||= begin
+      ruby = File.join(Config::CONFIG['bindir'], Config::CONFIG['ruby_install_name'])
+      ruby << Config::CONFIG['EXEEXT']
+
+      # escape string in case path to ruby executable contain spaces.
+      ruby.sub!(/.*\s.*/m, '"\&"')
+      ruby
+    end
+  end
+end
--- padrino-gen/lib/padrino-gen.rb
+++ padrino-gen/lib/padrino-gen.rb
@@ -2,6 +2,15 @@ require 'padrino-core/tasks'
 
 module Padrino
   ##
+  # This method return the correct location of padrino-gen bin or
+  # exec it using Kernel#system with the given args
+  #
+  def self.bin_gen(*args)
+    @_padrino_gen_bin ||= [Padrino.ruby_command, File.expand_path("../../bin/padrino-gen", __FILE__)]
+    args.empty? ? @_padrino_gen_bin : system(args.unshift(@_padrino_gen_bin).join(" "))
+  end
+
+  ##
   # This module it's used for register generators
   #
   # Can be useful for 3rd party generators:
