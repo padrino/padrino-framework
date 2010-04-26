b3ba7b42bf2dcc67e5ceee2c21f947b59cdf2f00
Florian Gilcher <flo@andersground.net>
2010-04-26 20:55:58 +0800

    Additional test for content handling

--- padrino-core/test/test_routing.rb
+++ padrino-core/test/test_routing.rb
@@ -100,6 +100,24 @@ class TestRouting < Test::Unit::TestCase
     get "/d.js"
     assert_equal "/d.js?foo=bar", body
   end
+  
+  should "not allow Accept-Headers it does not provide" do
+    mock_app do
+      get(:a, :provides => [:html, :js]){ content_type }
+    end
+    
+    get "/a", {}, {"HTTP_ACCEPT" => "application/yaml"}
+    assert_equal 404, status
+  end
+  
+  should "not default to HTML if HTML is not provided and no type is given" do
+    mock_app do
+      get(:a, :provides => [:js]){ content_type }
+    end
+    
+    get "/a", {}, {}
+    assert_equal 404, status
+  end
 
   should "generate routes for format simple" do
     mock_app do
