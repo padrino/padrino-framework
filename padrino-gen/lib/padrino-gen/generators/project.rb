19263b14ccc6ecd39393b0b07a905cc434b11b52
achiu <mr.arthur.chiu@gmail.com>
2010-05-18 16:05:56 -0700

    [padrino-gen] changes tiny gen aliases to not clash with --app aliases

--- padrino-gen/lib/padrino-gen/generators/project.rb
+++ padrino-gen/lib/padrino-gen/generators/project.rb
@@ -24,7 +24,7 @@ module Padrino
       class_option :run_bundle,   :desc => "Run bundle install",   :aliases => '-b', :default => false, :type => :boolean
       class_option :root,         :desc => "The root destination", :aliases => '-r', :default => ".",   :type => :string
       class_option :dev,          :desc => "Use padrino from a git checkout",        :default => false, :type => :boolean
-      class_option :tiny,         :desc => "Generate tiny app skeleton", :aliases => '-a', :default => false, :type => :boolean
+      class_option :tiny,         :desc => "Generate tiny app skeleton", :aliases => '-i', :default => false, :type => :boolean
 
       # Definitions for the available customizable components
       component_option :orm,        "database engine",    :aliases => '-d', :choices => [:activerecord, :datamapper, :mongomapper, :mongoid, :sequel, :couchrest], :default => :none
