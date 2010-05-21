7aa4316959c42d43c5f949e7857b95a5343e8145
Nathan Esquenazi <nesquena@gmail.com>
2010-05-20 23:53:19 -0700

    [padrino-core] Separated out comprehensive route tests into separate should blocks

--- padrino-core/test/test_routing.rb
+++ padrino-core/test/test_routing.rb
@@ -642,57 +642,56 @@ class TestRouting < Test::Unit::TestCase
     assert_equal 'foo;bar-whatever;baz', body
   end
 
-  should 'work with params' do
+  should 'work with arbitrary params' do
     mock_app do
-      controller :test1 do
-        get(:url1) do
-          params[:foo]
-        end
-        get(:url2, :provides => [:html, :json]) do
-          params[:foo]
-        end
-      end
-      controller :test2, :parent => :parent1, :parent1_id => 1 do
-        get(:url3) do
-          params[:foo]
-        end
-        
-        get(:url4, :with => :with1) do
-          params[:foo]
-        end
-        
-        get(:url5, :with => :with2, :provides => [:html]) do
-          params[:foo]
-        end
-      end
       get(:testing) { params[:foo] }
     end
-    
+
     url = @app.url(:testing, :foo => 'bar')
     assert_equal "/testing?foo=bar", url
     get url
     assert_equal "bar", body
+  end
+
+  should 'work with controller and arbitrary params' do
+    mock_app do
+      get(:testing) { params[:foo] }
+      controller :test1 do
+        get(:url1) { params[:foo] }
+        get(:url2, :provides => [:html, :json]) { params[:foo] }
+      end
+    end
     
     url = @app.url(:test1, :url1, :foo => 'bar1')
     assert_equal "/test1/url1?foo=bar1", url
     get url
     assert_equal "bar1", body
-    
+
     url = @app.url(:test1, :url2, :foo => 'bar2')
     assert_equal "/test1/url2?foo=bar2", url
     get url
     assert_equal "bar2", body
-    
+  end
+
+  should "work with params and parent options" do
+    mock_app do
+      controller :test2, :parent => :parent1, :parent1_id => 1 do
+        get(:url3) { params[:foo] }
+        get(:url4, :with => :with1) { params[:foo] }
+        get(:url5, :with => :with2, :provides => [:html]) { params[:foo] }
+      end
+    end
+
     url = @app.url(:test2, :url3, :foo => 'bar3')
     assert_equal "/parent1/1/test2/url3?foo=bar3", url
     get url
     assert_equal "bar3", body
-    
+
     url = @app.url(:test2, :url4, :with1 => 'awith1', :foo => 'bar4')
     assert_equal "/parent1/1/test2/url4/awith1?foo=bar4", url
     get url
     assert_equal "bar4", body
-    
+
     url = @app.url(:test2, :url5, :with2 => 'awith1', :foo => 'bar5')
     assert_equal "/parent1/1/test2/url5/awith1?foo=bar5", url
     get url
