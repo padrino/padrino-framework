ea29f505a299598a1f6dae1d8f8f01b834a5c6f5
Nathan Esquenazi <nesquena@gmail.com>
2010-05-25 15:38:24 -0700

    [padrino-gen] Updates mootools script to include elements.delegates

--- padrino-gen/lib/padrino-gen/generators/templates/scripts/mootools-core.js
+++ padrino-gen/lib/padrino-gen/generators/templates/scripts/mootools-core.js
@@ -355,3 +355,83 @@ b.html=d.stripScripts(function(e){b.javascript=e;});var a=this.processHTML(b.htm
 }return this.retrieve("load");}};Element.implement({load:function(){this.get("load").send(Array.link(arguments,{data:Object.type,url:String.type}));return this;
 }});Request.JSON=new Class({Extends:Request,options:{secure:true},initialize:function(a){this.parent(a);this.headers.extend({Accept:"application/json","X-Request":"JSON"});
 },success:function(a){this.response.json=JSON.decode(a,this.options.secure);this.onSuccess(this.response.json,a);}});
+
+//  ----------- Delegation -----------
+/*
+---
+description: Simpler event delegation for MooTools.
+ 
+license: MIT-style
+ 
+authors:
+- Arian Stolwijk
+ 
+requires:
+  core/1.2.4:
+  - Element.Event
+  - Selectors
+ 
+provides: [Element.addDelegate, Element.addDelegates, Element.removeDelegate, Element.removeDelegates,Element.Properties.delegates.set]
+ 
+...
+*/
+
+
+Element.Properties.delegates = {
+	set: function(delegates){
+		this.addDelegates(delegates);
+	}
+};
+
+Element.implement({
+	
+	addDelegate: function(type,selector,fn){
+		var elmt = document.id(this);
+				
+		var delegatefn = function(e){
+			var target = document.id(e.target);
+			elmt.getElements(selector).each(function(el){
+				if(el === target) fn.apply(el,[e]);
+			});
+		};
+		
+		var stores = elmt.retrieve('delegates') || {};
+		if(!stores[type]) stores[type] = {};
+		if(!stores[type][selector]) stores[type][selector] = [];
+		stores[type][selector].push([delegatefn,fn]);
+
+		return elmt.addEvent(type,delegatefn).store('delegates',stores);
+	},
+	
+	addDelegates: function(types){
+		for(var type in types){
+			for(var selector in types[type])
+				this.addDelegate(type,selector,types[type][selector]);
+		}
+		return this;
+	},
+	
+	removeDelegate: function(type,selector,fn){
+		var elmt = document.id(this);
+		
+		var stores = this.retrieve('delegates');
+		if(!stores[type] || !stores[type][selector]) return elmt;
+		
+		$each(stores[type][selector],function(fns,i){
+			if(fns[1] === fn){
+				elmt.removeEvent(type,fns[0]);
+				delete stores[type][selector][i];
+			}
+		});
+		return elmt;
+	},
+	
+	removeDelegates: function(types){
+		for(var type in types){
+			for(var selector in types[type])
+				this.removeDelegate(type,selector,types[type][selector]);
+		}
+		return this;		
+	}
+	
+});
