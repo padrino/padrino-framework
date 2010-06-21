c0142fadeef3b2c2ccafeacdc4512c81dbd16367
Yannick Koechlin <yannick@koechlin.info>
2010-06-21 15:35:48 +0800

    fixes #221.

--- padrino-helpers/lib/padrino-helpers/format_helpers.rb
+++ padrino-helpers/lib/padrino-helpers/format_helpers.rb
@@ -171,8 +171,8 @@ module Padrino
       def distance_of_time_in_words(from_time, to_time = 0, include_seconds = false, options = {})
         from_time = from_time.to_time if from_time.respond_to?(:to_time)
         to_time = to_time.to_time if to_time.respond_to?(:to_time)
-        distance_in_minutes = (((to_time - from_time).abs)/60).round
-        distance_in_seconds = ((to_time - from_time).abs).round
+        distance_in_minutes = (((to_time.to_i - from_time.to_i).abs)/60).round
+        distance_in_seconds = ((to_time.to_i - from_time.to_i).abs).round
 
         I18n.with_options :locale => options[:locale], :scope => :'datetime.distance_in_words' do |locale|
           case distance_in_minutes
