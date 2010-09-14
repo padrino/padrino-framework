c648be04ebb5b6f589f697c2274840ffd807fb5c
Kriss Kowalik <kriss.kowalik@gmail.com>
2010-09-14 21:32:17 +0200

    Added DomHelpers line in #registered

--- padrino-helpers/lib/padrino-helpers.rb
+++ padrino-helpers/lib/padrino-helpers.rb
@@ -30,6 +30,7 @@ module Padrino
     class << self
       def registered(app)
         app.set :default_builder, 'StandardFormBuilder'
+        app.helpers Padrino::Helpers::DomHelpers
         app.helpers Padrino::Helpers::OutputHelpers
         app.helpers Padrino::Helpers::TagHelpers
         app.helpers Padrino::Helpers::AssetTagHelpers
