64e3d1021c7a64da234aefcfc58c904856d16c32
lenzcom <dlenz@lenzcom.de>
2010-12-19 10:29:38 +0100

    compass compass-component to use a config file for project generation

--- padrino-gen/lib/padrino-gen/generators/components/stylesheets/compass.rb
+++ padrino-gen/lib/padrino-gen/generators/components/stylesheets/compass.rb
@@ -7,17 +7,7 @@ module CompassInitializer
   def self.registered(app)
     require 'sass/plugin/rack'
 
-    Compass.configuration do |config|
-      config.project_path = Padrino.root
-      config.sass_dir = "app/stylesheets"
-      config.project_type = :stand_alone
-      config.http_path = "/"
-      config.css_dir = "public/stylesheets"
-      config.images_dir = "public/images"
-      config.javascripts_dir = "public/javascripts"
-      config.output_style = :compressed
-    end
-
+    Compass.add_project_configuration './config/compass.config'
     Compass.configure_sass_plugin!
     Compass.handle_configuration_change!
 
@@ -34,6 +24,8 @@ def setup_stylesheet
   require_dependencies 'compass'
   create_file destination_root('/lib/compass_plugin.rb'), COMPASS_INIT
   inject_into_file destination_root('/app/app.rb'), COMPASS_REGISTER, :after => "register Padrino::Helpers\n"
-
+  
+  copy_file "components/stylesheets/compass/compass.config", destination_root('/config/compass.config')
+  remove_file "components/stylesheets/compass/compass.config"
   directory "components/stylesheets/compass/", destination_root('/app/stylesheets')
 end
