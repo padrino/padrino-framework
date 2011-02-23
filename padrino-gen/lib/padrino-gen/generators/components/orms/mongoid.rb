b7a9d25c1e74ed25cb3f29754d9b1195a9cba697
Arthur Chiu <Mr.Arthur.Chiu@gmail.com>
2011-02-22 22:37:52 -0800

    [padrino-gen] update mongoid adapter to use 2.0.0.beta.20

--- padrino-gen/lib/padrino-gen/generators/components/orms/mongoid.rb
+++ padrino-gen/lib/padrino-gen/generators/components/orms/mongoid.rb
@@ -28,7 +28,7 @@ MONGO
 
 def setup_orm
   require_dependencies 'bson_ext', :require => 'mongo'
-  require_dependencies 'mongoid', '2.0.0.rc.7'
+  require_dependencies 'mongoid', '2.0.0.beta.20'
   create_file("config/database.rb", MONGOID.gsub(/!NAME!/, @app_name.underscore))
   empty_directory('app/models')
 end
