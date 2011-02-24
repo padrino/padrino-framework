da95b33e9eab9891215408f1916148caa812296e
Arthur Chiu <Mr.Arthur.Chiu@gmail.com>
2011-02-23 21:17:46 -0800

    [padrino-gen] remove legacy bundler setup from config/boot.rb

--- padrino-gen/lib/padrino-gen/generators/project/config/boot.rb
+++ padrino-gen/lib/padrino-gen/generators/project/config/boot.rb
@@ -1,17 +1,12 @@
 # Defines our constants
 PADRINO_ENV  = ENV["PADRINO_ENV"] ||= ENV["RACK_ENV"] ||= "development"  unless defined?(PADRINO_ENV)
 PADRINO_ROOT = File.expand_path(File.join(File.dirname(__FILE__), '..')) unless defined?(PADRINO_ROOT)
-
-begin
-  # Require the preresolved locked set of gems.
-  require File.expand_path('../../.bundle/environment', __FILE__)
-rescue LoadError
-  # Fallback on doing the resolve at runtime.
-  require 'rubygems'
-  require 'bundler'
-  Bundler.setup
-end
-
+# Load Bundler
+require 'rubygems'
+require 'bundler'
+# Only have default and environemtn gems
+Bundler.setup(:default, PADRINO_ENV.to_sym)
+# Only require default and environment gems
 Bundler.require(:default, PADRINO_ENV.to_sym)
 puts "=> Located #{Padrino.bundle} Gemfile for #{Padrino.env}"
 
