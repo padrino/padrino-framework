a131808c391f6562040c9fe5ea25090d14089def
Steven Garcia <stevendgarcia@gmail.com>
2011-03-14 22:23:39 +0100

    Select tag now takes :grouped_options

--- padrino-helpers/lib/padrino-helpers/form_helpers.rb
+++ padrino-helpers/lib/padrino-helpers/form_helpers.rb
@@ -274,7 +274,11 @@ module Padrino
         options[:options] = options_from_collection(collection, fields) if collection
         blank = options.delete(:include_blank)
         options[:options].to_a.unshift(blank.is_a?(String) ? [blank, ''] : '') if blank
-        select_options_html = options_for_select(options.delete(:options), options.delete(:selected))
+        select_options_html = if options[:options]
+          options_for_select(options.delete(:options), options.delete(:selected))
+        elsif options[:grouped_options]
+          grouped_options_for_select(options.delete(:grouped_options), options.delete(:selected))
+        end
         options.merge!(:name => "#{options[:name]}[]") if options[:multiple]
         content_tag(:select, select_options_html, options)
       end
@@ -366,6 +370,27 @@ module Padrino
         option_items.collect do |caption, value|
           value ||= caption
           content_tag(:option, caption, :value => value, :selected => option_is_selected?(value, caption, selected_value))
+        end
+      end
+      
+      #
+      # Returns the optgroups with options tags for a select based on the given :grouped_options items
+      #
+      def grouped_options_for_select(collection,selected=nil)
+        if collection.is_a?(Hash)
+          collection.each.map do |key, value|
+            content_tag :optgroup, :label => key do
+              options_for_select(value, selected)
+            end
+          end
+        elsif collection.is_a?(Array)
+          collection.map do |optgroup|
+            content_tag :optgroup, :label => optgroup.first do
+              options_for_select(optgroup.last, selected)
+            end
+          end
+        else
+          raise "options must be a hash or array, not a #{collection.class}"
         end
       end
 
