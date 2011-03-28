b9b554f981750c942088159cf0f258834c46417d
Davide D'Agostino <d.dagostino@lipsiasoft.com>
2011-03-28 18:11:47 +0200

    [padrino-helpers] Fix ujs scripts creation

--- padrino-gen/lib/padrino-gen/generators/components/scripts/mootools.rb
+++ padrino-gen/lib/padrino-gen/generators/components/scripts/mootools.rb
@@ -1,5 +1,5 @@
 def setup_script
-  get('https://github.com/padrino/padrino-static/raw/master/js/mootools.js', destination_root("/public/javascripts/mootools.js"))
+  get('https://github.com/padrino/padrino-static/raw/master/js/mootools.js', destination_root("/public/javascripts/mootools-core.js"))
   get('https://github.com/padrino/padrino-static/raw/master/ujs/mootools-ujs.js', destination_root("/public/javascripts/mootools-ujs.js"))
   create_file(destination_root('/public/javascripts/application.js'), "// Put your application scripts here")
 end
