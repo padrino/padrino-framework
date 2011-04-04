a84e9d8a2ea52b4807d29d2786d1d3e03553b48b
Nathan Esquenazi <nesquena@gmail.com>
2011-04-03 22:11:17 -0700

    Revert "Removed from tests erubis."
    This reverts commit 496c606eca1956e2d181456b55eba321cdca0dac.

--- padrino-gen/test/test_project_generator.rb
+++ padrino-gen/test/test_project_generator.rb
@@ -330,6 +330,12 @@ class TestProjectGenerator < Test::Unit::TestCase
       assert_match_in_file(/gem 'haml'/, "#{@apptmp}/sample_project/Gemfile")
     end
 
+    should "properly generate for erubis" do
+      buffer = silence_logger { generate(:project, 'sample_project', "--root=#{@apptmp}", '--renderer=erubis','--script=none') }
+      assert_match(/Applying.*?erubis.*?renderer/,buffer)
+      assert_match_in_file(/gem 'erubis'/, "#{@apptmp}/sample_project/Gemfile")
+    end
+
     should "properly generate for liquid" do
       buffer = silence_logger { generate(:project, 'sample_project', "--root=#{@apptmp}", '--renderer=liquid','--script=none') }
       assert_match(/Applying.*?liquid.*?renderer/,buffer)
