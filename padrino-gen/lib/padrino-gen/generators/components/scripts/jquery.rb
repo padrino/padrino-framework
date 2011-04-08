7625ade4c5dd50b574a1631f78cb6264a5f8494d
Davide D'Agostino <d.dagostino@lipsiasoft.com>
2011-04-08 16:09:29 +0200

    [padrino-gen] Use https://github.com/padrino/padrino-static for download js and ujs adapters (if no internet connection use a local copy)

--- padrino-gen/lib/padrino-gen/generators/components/scripts/jquery.rb
+++ padrino-gen/lib/padrino-gen/generators/components/scripts/jquery.rb
@@ -1,5 +1,10 @@
 def setup_script
-  get('https://github.com/padrino/padrino-static/raw/master/js/jquery.js', destination_root("/public/javascripts/jquery.js"))
-  get('https://github.com/padrino/padrino-static/raw/master/ujs/jquery-ujs.js', destination_root("/public/javascripts/jquery-ujs.js"))
+  begin
+    get('https://github.com/padrino/padrino-static/raw/master/js/jquery.js',  destination_root("/public/javascripts/jquery.js"))
+    get('https://github.com/padrino/padrino-static/raw/master/ujs/jquery.js', destination_root("/public/javascripts/jquery-ujs.js"))
+  rescue
+    copy_file('templates/static/js/jquery.js',  destination_root("/public/javascripts/jquery.js"))
+    copy_file('templates/static/ujs/jquery.js', destination_root("/public/javascripts/jquery-ujs.js"))
+  end
   create_file(destination_root('/public/javascripts/application.js'), "// Put your application scripts here")
 end
--- padrino-gen/lib/padrino-gen/generators/components/scripts/mootools.rb
+++ padrino-gen/lib/padrino-gen/generators/components/scripts/mootools.rb
@@ -1,5 +1,10 @@
 def setup_script
-  get('https://github.com/padrino/padrino-static/raw/master/js/mootools.js', destination_root("/public/javascripts/mootools.js"))
-  get('https://github.com/padrino/padrino-static/raw/master/ujs/mootools-ujs.js', destination_root("/public/javascripts/mootools-ujs.js"))
+  begin
+    get('https://github.com/padrino/padrino-static/raw/master/js/mootools.js',  destination_root("/public/javascripts/mootools.js"))
+    get('https://github.com/padrino/padrino-static/raw/master/ujs/mootools.js', destination_root("/public/javascripts/mootools-ujs.js"))
+  rescue
+    copy_file('templates/static/js/mootools.js',  destination_root("/public/javascripts/mootools.js"))
+    copy_file('templates/static/ujs/mootools.js', destination_root("/public/javascripts/mootools-ujs.js"))
+  end
   create_file(destination_root('/public/javascripts/application.js'), "// Put your application scripts here")
 end
