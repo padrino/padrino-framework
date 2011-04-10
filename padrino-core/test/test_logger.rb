d7126a3a645e95f0a701ce334313a7ee5d398c9d
Jacques Crocker <railsjedi@gmail.com>
2011-04-11 03:35:05 +0800

    [padrino-core] Added setting log_static setting to Padrino.logger (defaults to false). When true, Padrino will log http requests for static assets.

--- padrino-core/test/test_logger.rb
+++ padrino-core/test/test_logger.rb
@@ -51,5 +51,22 @@ class TestPadrinoLogger < Test::Unit::TestCase
       assert_match /GET/, Padrino.logger.log.string
     end
 
+    context "static asset logging" do
+      should 'not log static assets by default' do
+        mock_app { get("/images/something.png"){ env["sinatra.static_file"] = '/public/images/something.png'; "Foo" } }
+        get "/images/something.png"
+        assert_equal "Foo", body
+        assert_match "", Padrino.logger.log.string
+      end
+
+      should 'allow turning on static assets logging' do
+        Padrino.logger.instance_eval{ @log_static = true }
+        mock_app { get("/images/something.png"){ env["sinatra.static_file"] = '/public/images/something.png'; "Foo" } }
+        get "/images/something.png"
+        assert_equal "Foo", body
+        assert_match /GET/, Padrino.logger.log.string
+        Padrino.logger.instance_eval{ @log_static = false }
+      end
+    end
   end
 end
