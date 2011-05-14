0b06bf6f7933b3aa0949103abb490598808714ad
Davide D'Agostino <d.dagostino@lipsiasoft.com>
2011-05-14 21:26:42 +0200

    [padrino-core] Prevent warnings.

--- padrino-core/lib/padrino-core/application/rendering.rb
+++ padrino-core/lib/padrino-core/application/rendering.rb
@@ -17,12 +17,12 @@ module Padrino
     #
     IGNORE_FILE_PATTERN = [
       /~$/ # This is for Gedit
-    ]
+    ] unless defined?(IGNORE_FILE_PATTERN)
 
     ##
     # Default rendering options used in the #render-method
     #
-    DEFAULT_RENDERING_OPTIONS = { :strict_format => false, :raise_exceptions => true }
+    DEFAULT_RENDERING_OPTIONS = { :strict_format => false, :raise_exceptions => true } unless defined?(DEFAULT_RENDERING_OPTIONS)
 
     ##
     # Main class that register this extension
