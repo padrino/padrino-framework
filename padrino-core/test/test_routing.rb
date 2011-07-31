85a9dd2296330308423fe5c58e4b4d0ab2d23439
Paul Nikitochkin <pftg@jetthoughts.com>
2011-07-31 15:10:06 +0300

    Instead of hard parsinf MIME type from the path, use param[:format] to remove duplication of logic.

--- padrino-core/test/test_routing.rb
+++ padrino-core/test/test_routing.rb
@@ -363,7 +363,7 @@ class TestRouting < Test::Unit::TestCase
     assert_equal ":json", body
   end
 
-  should "correctly accept . in route" do
+  should "correctly accept '.' in the route" do
     mock_app do
       get "test.php", :provides => [:html, :json] do
         content_type.inspect
@@ -373,9 +373,23 @@ class TestRouting < Test::Unit::TestCase
     assert_equal ":html", body
     get "/test.php.json"
     assert_equal ":json", body
-    get "/test.php?format=json"
-    assert_equal ":json", body
+  end
 
+  should "correctly accept priority of format" do
+    mock_app do
+      get "test.php", :provides => [:html, :json, :xml] do
+        content_type.inspect
+      end
+    end
+
+    get "/test.php"
+    assert_equal ":html", body
+    get "/test.php", {}, { 'HTTP_ACCEPT' => 'application/xml' }
+    assert_equal ":xml", body
+    get "/test.php?format=json", { 'HTTP_ACCEPT' => 'application/xml' }
+    assert_equal ":json", body
+    get "/test.php.json?format=html", { 'HTTP_ACCEPT' => 'application/xml' }
+    assert_equal ":json", body
   end
 
   should "generate routes for format with controller" do
