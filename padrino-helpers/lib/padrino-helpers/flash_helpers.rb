bd534f93d90c773df2f0001ff63694822a55b715
Nathan Esquenazi <nesquena@gmail.com>
2011-09-25 14:43:47 -0700

    Revert "Alias method chain seems that doesn't play well in some circumstances."
    This reverts commit 1ebb7082106953d816a914b69cd08a4c61d0ddec.

--- padrino-helpers/lib/padrino-helpers/flash_helpers.rb
+++ padrino-helpers/lib/padrino-helpers/flash_helpers.rb
@@ -2,16 +2,17 @@ module Padrino
   module Helpers
     module FlashHelpers
       def self.included(base)
-        base.before { @_flash, session[:_flash] = session[:_flash], nil if settings.sessions? && session[:_flash]; true }
+        base.before { @_flash, session[:_flash] = session[:_flash], nil if settings.sessions? && session[:_flash] }
+        base.alias_method_chain :redirect, :flash
       end
 
       def flash
         @_flash ||= {}
       end
 
-      def redirect(uri, *args)
+      def redirect_with_flash(uri, *args)
         session[:_flash] = flash if settings.sessions? && flash.present?
-        super(uri, *args)
+        redirect_without_flash(uri, *args)
       end
     end # FlashHelpers
   end # Helpers
