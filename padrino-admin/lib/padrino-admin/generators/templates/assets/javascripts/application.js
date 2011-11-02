01431fd1a0767838824ba98597a880cb6917dfeb
Carlo Bertini <waydotnet@gmail.com>
2011-11-02 17:07:52 +0100

    refactor code
    tanks @daddye

--- padrino-admin/lib/padrino-admin/generators/templates/assets/javascripts/application.js
+++ padrino-admin/lib/padrino-admin/generators/templates/assets/javascripts/application.js
@@ -9,7 +9,4 @@ $(function(){
     keyboard: true,
     backdrop: true
   });
-  $('#save_and_continue').click(function(){
-    $('<input/>',{type:'hidden',id:'s_c',value:'true', name:'s_c'}).appendTo($(this).closest("form"));
-  });
 });
