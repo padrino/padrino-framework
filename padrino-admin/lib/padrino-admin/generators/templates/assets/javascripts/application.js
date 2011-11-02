7208db8eff39c22b2f6f79463b4c837f80dfacf6
Carlo Bertini <waydotnet@gmail.com>
2011-11-02 14:33:52 +0100

    add save_and_continue button
    Add save_and_continue button….pls check language translate :D:D:D

--- padrino-admin/lib/padrino-admin/generators/templates/assets/javascripts/application.js
+++ padrino-admin/lib/padrino-admin/generators/templates/assets/javascripts/application.js
@@ -9,4 +9,7 @@ $(function(){
     keyboard: true,
     backdrop: true
   });
+  $('#save_and_continue').click(function(){
+    $('<input/>',{type:'hidden',id:'s_c',value:'true', name:'s_c'}).appendTo($(this).closest("form"));
+  });
 });
