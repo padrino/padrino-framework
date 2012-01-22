b2f3738b7a6303cd679d9f48b0c925d9cd7d5ea8
Benjamin Bloch <cirex@gamesol.org>
2012-01-21 20:08:26 -0500

    YARD updates

--- padrino-helpers/lib/padrino-helpers/tag_helpers.rb
+++ padrino-helpers/lib/padrino-helpers/tag_helpers.rb
@@ -1,12 +1,12 @@
-module Padrino
+﻿module Padrino
   module Helpers
     ##
     # Helpers related to producing html tags within templates.
-    #
+    ##
     module TagHelpers
       ##
       # Tag values escaped to html entities
-      #
+      ##
       ESCAPE_VALUES = {
         "<" => "&lt;",
         ">" => "&gt;",
@@ -25,26 +25,63 @@ module Padrino
         :muted,
         :readonly,
         :required,
-        :selected,
+        :selected
       ]
 
       ##
-      # Creates an html tag with given name, content and options
+      # Creates an HTML tag with given name, content, and options
       #
-      # @overload content_tag(name, content, options)
-      #   @param [Symbol]  name     The html type of tag.
-      #   @param [String]  content  The contents in the tag.
-      #   @param [Hash]    options  The html options to include in this tag.
-      # @overload content_tag(name, options, &block)
-      #   @param [Symbol]  name     The html type of tag.
-      #   @param [Hash]    options  The html options to include in this tag.
-      #   @param [Proc]    block    The block returning html content
+      # @overload content_tag(name, content, options = nil)
+      #   @param [Symbol] name
+      #     The name of the HTML tag to create.
+      #   @param [String] content
+      #     The content inside of the the tag.
+      #   @param [Hash] options
+      #     The HTML options to include in this tag.
       #
-      # @return [String] The html generated for the tag.
+      # @overload content_tag(name, options = nil, &block)
+      #   @param [Symbol] name
+      #     The name of the HTML tag to create.
+      #   @param [Hash] options
+      #     The HTML options to include in this tag.
+      #   @param [Proc] block
+      #     The block returning HTML content.
+      #
+      # @macro [new] global_html_attributes
+      #   @option options [String] :id
+      #     Specifies a unique identifier for the element.
+      #   @option options [String] :class
+      #     Specifies the stylesheet class of the element.
+      #   @option options [String] :title
+      #     Specifies the title for the element.
+      #   @option options [String] :accesskey
+      #     Specifies a shortcut key to access the element.
+      #   @option options [Symbol] :dropzone
+      #     Specifies what happens when dragged items are dropped on the element. (:copy, :link, :move)
+      #   @option options [Boolean] :hidden
+      #     Specifies whether or not the element is hidden from view.
+      #   @option options [Boolean] :draggable
+      #     Specifies whether or not the element is draggable.
+      #   @option options [Boolean] :contenteditable
+      #     Specifies whether or not the element is editable.
+      #
+      # @return [String]
+      #   Generated HTML with specified +options+
       #
       # @example
-      #   content_tag(:p, "hello", :class => 'light')
-      #   content_tag(:p, :class => 'dark') { ... }
+      #   content_tag(:p, 'Hello World', :class => 'light')
+      #
+      #   # => <p class="light">
+      #   # =>   Hello World
+      #   # => </p>
+      #
+      #   content_tag(:p, :class => 'dark') do
+      #     link_to 'Padrino', 'http://www.padrinorb.com'
+      #   end
+      #
+      #   # => <p class="dark">
+      #   # =>   <a href="http://www.padrinorb.com">Padrino</a>
+      #   # => </p>
       #
       # @api public
       def content_tag(name, content = nil, options = nil, &block)
@@ -60,18 +97,57 @@ module Padrino
       end
 
       ##
-      # Creates an html input field with given type and options
+      # Creates an HTML input field with the given type and options
       #
       # @param [Symbol] type
-      #   The html type of tag to create.
+      #   The type of input to create.
       # @param [Hash] options
-      #   The html options to include in this tag.
+      #   The HTML options to include in this input.
       #
-      # @return [String] The html for the input tag.
+      # @option options [String] :id
+      #   Specifies a unique identifier for the input.
+      # @option options [String] :class
+      #   Specifies the stylesheet class of the input.
+      # @option options [String] :name
+      #   Specifies the name of the input.
+      # @option options [String] :accesskey
+      #   Specifies a shortcut key to access the input.
+      # @option options [Integer] :tabindex
+      #   Specifies the tab order of the input.
+      # @option options [Boolean] :hidden
+      #   Specifies whether or not the input is hidden from view.
+      # @option options [Boolean] :spellcheck
+      #   Specifies whether or not the input should have it's spelling and grammar checked for errors.
+      # @option options [Boolean] :draggable
+      #   Specifies whether or not the input is draggable.
+      # @option options [String] :pattern
+      #   Specifies the regular expression pattern that the input's value is checked against.
+      # @option options [Symbol] :autocomplete
+      #   Specifies whether or not the input should have autocomplete enabled. (:on, :off)
+      # @option options [Boolean] :autofocus
+      #   Specifies whether or not the input should automatically get focus when the page loads.
+      # @option options [Boolean] :required
+      #   Specifies whether or not the input is required to be completeled before the form is submitted.
+      # @option options [Boolean] :readonly
+      #   Specifies whether or not the input is read only.
+      # @option options [Boolean] :disabled
+      #   Specifies whether or not the input is disabled.
+      #
+      # @return [String]
+      #   Generated HTML with specified +options+
       #
       # @example
-      #   input_tag :text, :class => "test"
-      #   input_tag :password, :size => "20"
+      #   input_tag :text, :name => 'handle'
+      #   # => <input type="test" name="handle">
+      #
+      #   input_tag :password, :name => 'password', :size => 20
+      #   # => <input type="password" name="password" size="20">
+      #
+      #   input_tag :text, :name => 'username', :required => true, :autofocus => true
+      #   # => <input type="text" name="username" required autofocus>
+      #
+      #   input_tag :number, :name => 'credit_card', :autocomplete => :off
+      #   # => <input type="number" autocomplete="off">
       #
       # @api semipublic
       def input_tag(type, options = {})
@@ -80,17 +156,27 @@ module Padrino
       end
 
       ##
-      # Creates an html tag with the given name and options
+      # Creates an HTML tag with the given name and options
       #
-      # @param [Symbol] type
-      #   The html type of tag to create.
+      # @param [Symbol] name
+      #  The name of the HTML tag to create.
       # @param [Hash] options
-      #   The html options to include in this tag.
+      #  The HTML options to include in this tag.
       #
-      # @return [String] The html for the input tag.
+      # @macro global_html_attributes
+      #
+      # @return [String]
+      #   Generated HTML with specified +options+
       #
       # @example
-      #   tag(:br, :style => 'clear:both')
+      #   tag :hr, :class => 'dotted'
+      #   # => <hr class="dotted">
+      #
+      #   tag :input, :name => 'username', :type => :text
+      #   # => <input name="username" type="text">
+      #
+      #   tag :img, :src => 'images/pony.jpg', :alt => 'My Little Pony'
+      #   # => <img src="images/pony.jpg" alt="My Little Pony">
       #
       # @api public
       def tag(name, options = nil, open = false)
@@ -100,7 +186,7 @@ module Padrino
       private
         ##
         # Returns a compiled list of HTML attributes
-        #
+        ##
         def tag_options(options)
           unless options.blank?
             attributes = []
@@ -119,7 +205,7 @@ module Padrino
 
         ##
         # Escape tag values to their HTML/XML entities.
-        #
+        ##
         def escape_value(string)
           string.to_s.gsub(Regexp.union(*ESCAPE_VALUES.keys)){|c| ESCAPE_VALUES[c] }
         end
