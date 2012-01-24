ca76e2518fdeb6579f56c871dd016228727e1214
Davide D'Agostino <d.dagostino@lipsiasoft.com>
2012-01-24 10:55:47 +0100

    Updated stylesheets initializers to respect spaces.

--- padrino-gen/lib/padrino-gen/generators/components/stylesheets/sass.rb
+++ padrino-gen/lib/padrino-gen/generators/components/stylesheets/sass.rb
@@ -1,11 +1,11 @@
-SASS_INIT = (<<-SASS).gsub(/^/, '    ')
-# Enables support for SASS template reloading in rack applications.
-# See http://nex-3.com/posts/88-sass-supports-rack for more details.
-# Store SASS files (by default) within 'app/stylesheets'
-require 'sass/plugin/rack'
-Sass::Plugin.options[:template_location] = Padrino.root("app/stylesheets")
-Sass::Plugin.options[:css_location] = Padrino.root("public/stylesheets")
-app.use Sass::Plugin::Rack
+SASS_INIT = <<-SASS unless defined?(SASS_INIT)
+    # Enables support for SASS template reloading in rack applications.
+    # See http://nex-3.com/posts/88-sass-supports-rack for more details.
+    # Store SASS files (by default) within 'app/stylesheets'
+    require 'sass/plugin/rack'
+    Sass::Plugin.options[:template_location] = Padrino.root("app/stylesheets")
+    Sass::Plugin.options[:css_location] = Padrino.root("public/stylesheets")
+    app.use Sass::Plugin::Rack
 SASS
 
 def setup_stylesheet
--- padrino-gen/lib/padrino-gen/generators/components/stylesheets/scss.rb
+++ padrino-gen/lib/padrino-gen/generators/components/stylesheets/scss.rb
@@ -1,12 +1,12 @@
-SCSS_INIT = (<<-SCSS).gsub(/^/, '    ')
-# Enables support for SCSS template reloading in rack applications.
-# See http://nex-3.com/posts/88-sass-supports-rack for more details.
-# Store SCSS files (by default) within 'app/stylesheets'
-require 'sass/plugin/rack'
-Sass::Plugin.options[:syntax] = :scss
-Sass::Plugin.options[:template_location] = Padrino.root("app/stylesheets")
-Sass::Plugin.options[:css_location] = Padrino.root("public/stylesheets")
-app.use Sass::Plugin::Rack
+SCSS_INIT = <<-SCSS unless defined?(SCSS_INIT)
+    # Enables support for SCSS template reloading in rack applications.
+    # See http://nex-3.com/posts/88-sass-supports-rack for more details.
+    # Store SCSS files (by default) within 'app/stylesheets'
+    require 'sass/plugin/rack'
+    Sass::Plugin.options[:syntax] = :scss
+    Sass::Plugin.options[:template_location] = Padrino.root("app/stylesheets")
+    Sass::Plugin.options[:css_location] = Padrino.root("public/stylesheets")
+    app.use Sass::Plugin::Rack
 SCSS
 
 def setup_stylesheet
