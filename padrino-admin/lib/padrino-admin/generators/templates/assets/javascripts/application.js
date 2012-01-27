312a02e0b6823b3cd796627ea8cb8f659b5b9c74
Carlo Bertini <waydotnet@gmail.com>
2012-01-27 20:21:31 +0100

    modify slim template

--- padrino-admin/lib/padrino-admin/generators/templates/assets/javascripts/application.js
+++ padrino-admin/lib/padrino-admin/generators/templates/assets/javascripts/application.js
@@ -1,12 +1,37 @@
 $(function(){
-  $('table').dataTable({
-    StateSave: true,
-    aLengthMenu: [[50, 100, 150, -1], [50, 100, 150, 'All']],
-  });
 
-  $('.alert-message.modal').modal({
-    show: true,
-    keyboard: true,
-    backdrop: true
-  });
+    $('#field-errors').addClass('modal fade');
+    $('#field-errors h4').wrapAll("<div class='modal-header alert alert-error'></div>");
+    $('#field-errors .modal-header').prepend("<a href='#' class='close' data-dismiss='modal'> &times;</a>");
+    $('#field-errors p').wrap("<div class='modal-body'></div>") ;
+    $('#field-errors ul').wrap("<div class='modal-body'></div>") ;
+    $('#field-errors').append("<div class='modal-footer'><a href='#' class='btn primary' data-dismiss='modal'>Close</a></div>");
+    $('#field-errors').modal();
+
+    $('#flash-result').modal('show');
+
+
+    $('#deleter').on('hide', function () {
+        $('#deleter-title').empty();
+        $('#deleter .modal-body').empty();
+        $('#deleter-close').empty();
+    });
+
+    $(".btn_delete").click(function(){
+        // find form
+        var form ="."+ $(this).attr('data-form');
+        var clone = $(form).clone();
+        // remove hide class
+        $(clone).removeClass('hide');
+        // insert form into modal
+        $('#deleter .modal-body').append($(clone));
+        $('#deleter-title').append( $(form + ' input:submit').val() + ' : ' +$(form).attr('data-title') );
+        $('#deleter-close').append($(this).attr('data-cancel'));
+        $(clone).append($('#deleter-close').clone().removeClass('hide'));
+        $(clone).prepend("<h4>"+$(clone).attr('data-title')+"</h4");
+        $(clone).append("<div class='clearfix' />");
+        // show modal
+        $('#deleter').modal('show');
+
+    });
 });
