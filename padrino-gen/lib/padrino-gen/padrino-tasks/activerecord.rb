5c40be141a15b6e3fedece948a55a159a1a7432c
Jesse Farmer <jesse@20bits.com>
2012-02-10 19:02:06 -0800

    [Fix #784] ActiveRecord rake file should not prompt the user to log in as root if database authentication fails

--- padrino-gen/lib/padrino-gen/padrino-tasks/activerecord.rb
+++ padrino-gen/lib/padrino-gen/padrino-tasks/activerecord.rb
@@ -62,23 +62,10 @@ if defined?(ActiveRecord)
             ActiveRecord::Base.establish_connection(config.merge(:database => nil))
             ActiveRecord::Base.connection.create_database(config[:database], creation_options)
             ActiveRecord::Base.establish_connection(config)
-          rescue Mysql::Error => sqlerr
-            if sqlerr.errno == Mysql::Error::ER_ACCESS_DENIED_ERROR
-              print "#{sqlerr.error}. \nPlease provide the root password for your mysql installation\n>"
-              root_password = $stdin.gets.strip
-              grant_statement = "GRANT ALL PRIVILEGES ON #{config[:database]}.* " \
-                "TO '#{config[:username]}'@'localhost' " \
-                "IDENTIFIED BY '#{config[:password]}' WITH GRANT OPTION;"
-              ActiveRecord::Base.establish_connection(config.merge(
-                  :database => nil, :username => 'root', :password => root_password))
-              ActiveRecord::Base.connection.create_database(config[:database], creation_options)
-              ActiveRecord::Base.connection.execute grant_statement
-              ActiveRecord::Base.establish_connection(config)
-            else
-              $stderr.puts sqlerr.error
-              $stderr.puts "Couldn't create database for #{config.inspect}, charset: #{config[:charset] || @charset}, collation: #{config[:collation] || @collation}"
-              $stderr.puts "(if you set the charset manually, make sure you have a matching collation)" if config[:charset]
-            end
+          rescue
+            $stderr.puts $!
+            $stderr.puts "Couldn't create database for #{config.inspect}, charset: #{config[:charset] || @charset}, collation: #{config[:collation] || @collation}"
+            $stderr.puts "(if you set the charset manually, make sure you have a matching collation)" if config[:charset]
           end
         when 'postgresql'
           @encoding = config[:encoding] || ENV['CHARSET'] || 'utf8'
