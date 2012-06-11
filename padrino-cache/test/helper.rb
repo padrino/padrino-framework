048f7738b8c24b8dcf86dffa28f5a815a2bd5098
Davide D'Agostino <d.dagostino@lipsiasoft.com>
2012-06-11 11:55:40 +0200

    Fix gem loading problem with uuid and active support.

--- padrino-cache/test/helper.rb
+++ padrino-cache/test/helper.rb
@@ -3,9 +3,9 @@ PADRINO_ROOT = File.dirname(__FILE__) unless defined?(PADRINO_ROOT)
 
 require File.expand_path('../../../load_paths', __FILE__)
 require File.join(File.dirname(__FILE__), '..', '..', 'padrino-core', 'test', 'helper')
+require 'uuid'
 require 'padrino-cache'
 require 'fileutils'
-require 'uuid'
 
 class MiniTest::Spec
 
