962eaa78407eaa285df4e0bf219e6361bf9cd983
Nathan Esquenazi <nesquena@gmail.com>
2012-11-16 08:36:13 -0800

    [helpers] Rounding is built into 1.9.3

--- padrino-helpers/lib/padrino-helpers.rb
+++ padrino-helpers/lib/padrino-helpers.rb
@@ -4,10 +4,14 @@ require 'i18n'
 require 'enumerator'
 require 'active_support/time_with_zone'               # next extension depends on this
 require 'active_support/core_ext/string/conversions'  # to_date
-require 'active_support/core_ext/float/rounding'      # round
 require 'active_support/option_merger'                # with_options
 require 'active_support/core_ext/object/with_options' # with_options
 require 'active_support/inflector'                    # humanize
+begin
+  require 'active_support/core_ext/float/rounding'      # round
+rescue LoadError # built into 1.9.3
+  # do nothing  
+end
 
 FileSet.glob_require('padrino-helpers/**/*.rb', __FILE__)
 
