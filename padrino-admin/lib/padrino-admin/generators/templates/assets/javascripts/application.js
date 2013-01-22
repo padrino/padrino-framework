fe6dcc159802a21431c8ee8e57c4f8efe5ce8958
Darío Javier Cravero <dario@qinnova.com.ar>
2013-01-22 20:33:14 -0300

    - Implement delete selected - Coming next: massive classes/CSS cleanup.

--- padrino-admin/lib/padrino-admin/generators/templates/assets/javascripts/application.js
+++ padrino-admin/lib/padrino-admin/generators/templates/assets/javascripts/application.js
@@ -1,55 +1,80 @@
-(function() {
+!function($) {
   'use strict';
 
   $(function() {
+    var allCheckboxSelector = 'tbody tr td.selectable :checkbox', alertTimeout = 4000;
+
     // Automatically close alerts if there was any present.
     if ($('.alert').length > 0) {
-      setTimeout(function() { $('.alert').alert('close'); }, 3000);
+      setTimeout(function() { $('.alert').alert('close'); }, alertTimeout);
     }
 
     // Confirm before deleting one item 
     $('.delete-one').on('click', function(ev) {
       ev.preventDefault();
-      $(this).addClass('active').siblings('.popover-delete-one').first().show().find('.cancel').on('click', function() {
-        $(this).parents('.popover').hide().siblings('.delete-one').removeClass('active');
-      });
+      $(this).addClass('active').siblings('.popover-delete-one')
+        .first().show().find('.cancel').on('click', function() {
+
+          $(this).parents('.popover').hide().siblings('.delete-one').removeClass('active');
+        });
     });
 
-    // Check/uncheck all functionality
-    function idCheckboxes(table) {
-      return table.find('tbody tr td:first :checkbox');
+    function toggleAction(selector, disabled) {
+      var method = disabled ? 'addClass' : 'removeClass';
+      $(selector)[method]('disabled').parent()[method]('disabled');
     }
+
+    // Select/deselect record on row's click
+    $('#list tbody tr td').on('click', function(ev) {
+      ev.stopPropagation();
+      $(this).parent().find('.selectable :checkbox').click();
+    });
+    // Check/uncheck all functionality
     function checkAll(table, checked) {
       // Toggle all checkboxes on the table's body that exist on the first column.
-      idCheckboxes(table).attr('checked', checked ? 'checked' : false);
-
-      // Toggle delete selected functionality
-      // $('#delete-selected')[checked ? 'removeClass' : 'addClass']('disabled')
-      //   .parent()[checked ? 'removeClass' : 'addClass']('disabled');
+      table.find(allCheckboxSelector).prop('checked', checked);
+      toggleAction('#delete-selected', !checked);
     }
-    $('#check-all').on('click', function(ev) {
+    $('#select-all').on('click', function(ev) {
       ev.preventDefault();
+      // We assume we want to stay on the dropdown to delete all perhaps
+      ev.stopPropagation();
       checkAll($(this).parents('table'), true);
+      toggleAction('#select-all', true);
+      toggleAction('#deselect-all', false);
     });
-    $('#uncheck-all').on('click', function(ev) {
+    $('#deselect-all').on('click', function(ev) {
       ev.preventDefault();
       checkAll($(this).parents('table'), false);
+      toggleAction('#deselect-all', true);
+      toggleAction('#select-all', false);
     });
-
     // Delete selected
     $('#delete-selected').on('click', function(ev) {
       ev.preventDefault();
+      ev.stopPropagation();
       if ($(this).is('.disabled')) return;
 
-      console.log('TO DELETE', idCheckboxes($(this).parents('table')).map(function() { return $(this).val(); }).toArray());
+      $(this).parent().addClass('active').parent('.dropdown').addClass('open');
+      $(this).addClass('active').siblings('.popover-delete-selected').first().show().find('.cancel').on('click', function() {
+        // Hide the popover on cancel
+        $(this).parents('.popover').hide().siblings('#delete-selected').removeClass('active').parent().removeClass('active');
+        // and close the dropdown
+        $(this).parents('.dropdown').removeClass('open');
+      });
+
+      $(this).siblings('.popover-delete-selected').find(':hidden[data-delete-many-ids=true]').
+        val($(this).parents('table').find(allCheckboxSelector + ':checked').map(function() { return $(this).val(); }).toArray().join(','));
     });
 
-    $('#list tbody tr td:first :checkbox').on('click', function() {
-      var checked = $('#list tbody tr td:first :checkbox:checked').length > 0;
-      // Toggle delete selected functionality
-      $('#delete-selected')[checked ? 'removeClass' : 'addClass']('disabled')
-        .parent()[checked ? 'removeClass' : 'addClass']('disabled');
+    // Catch checkboxes check/uncheck and enable/disable the delete selected functionality
+    $('#list ' + allCheckboxSelector).on('click', function() {
+      var all = $('#list ' + allCheckboxSelector), checked = all.filter(':checked').length;
+
+      toggleAction('#delete-selected', checked === 0);
+      toggleAction('#deselect-all', checked === 0);
+      toggleAction('#select-all', checked === all.length);
     });
   });
 
-}).call(this);
+}(window.jQuery);
