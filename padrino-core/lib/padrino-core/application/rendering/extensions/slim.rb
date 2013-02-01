a2fe0eab669c39183524997edbfff2654708ca37
Florian Gilcher <florian.gilcher@asquera.de>
2013-02-01 21:03:06 +0100

    Register the Slim template as a SafeTemplate

--- padrino-core/lib/padrino-core/application/rendering/extensions/slim.rb
+++ padrino-core/lib/padrino-core/application/rendering/extensions/slim.rb
@@ -5,6 +5,10 @@ begin
     Padrino::Rendering.engine_configurations[:slim] =
       {:generator => Temple::Generators::RailsOutputBuffer,
        :buffer => "@_out_buf"}
+
+    class Slim::Template
+      include Padrino::Rendering::SafeTemplate
+    end
   end
 rescue LoadError
 end
