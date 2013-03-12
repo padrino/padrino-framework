2dbcc2d377be30e0daf493c12dfbb49cdbb74d63
Florian Gilcher <florian.gilcher@asquera.de>
2013-03-12 23:08:08 +0100

    Move rake initialization into CLI
    This avoids double-call scenarios of PadrinoTasks.init that lead
    to fake warnings and double-loading of tasks.
    
    Fixes #1109

--- padrino-core/lib/padrino-core/cli/rake.rb
+++ padrino-core/lib/padrino-core/cli/rake.rb
@@ -8,16 +8,8 @@ require 'padrino-gen'
 module PadrinoTasks
   def self.init(init=false)
     Padrino::Tasks.files.flatten.uniq.each { |rakefile| Rake.application.add_import(rakefile) rescue puts "<= Failed load #{ext}" }
-    if init
-      Rake.application.init
-      Rake.application.instance_variable_set(:@rakefile, __FILE__)
-      load(File.expand_path('../rake_tasks.rb', __FILE__))
-      Rake.application.load_imports
-      Rake.application.top_level
-    else
-      load(File.expand_path('../rake_tasks.rb', __FILE__))
-      Rake.application.load_imports
-    end
+    load(File.expand_path('../rake_tasks.rb', __FILE__))
+    Rake.application.load_imports
   end
 
   def self.use(task)
