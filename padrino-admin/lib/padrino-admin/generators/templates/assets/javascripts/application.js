933085856068008c3e8df172f93db323279b506f
Darío Javier Cravero <dario@qinnova.com.ar>
2013-03-20 23:00:08 -0300

    - Completely refactored the CSS structure to be simpler, more consistent and easier to follow. It rocks now! :) - As a little gem, I've added some media queries for iPads and the   Android Galaxy Nexus (Google phone v3). - Revamped the JavaScript admin so that it works better and consumes   even less resources. - Now I need to apply that structure to ERB and SLIM and we're good to   go! :)

--- padrino-admin/lib/padrino-admin/generators/templates/assets/javascripts/application.js
+++ padrino-admin/lib/padrino-admin/generators/templates/assets/javascripts/application.js
@@ -2,84 +2,114 @@
   'use strict';
 
   $(function() {
-    var allCheckboxSelector = 'tbody tr td.selectable :checkbox', alertTimeout = 4000;
+    function toggleAction(selector, disabled) {
+      var method = disabled ? 'addClass' : 'removeClass';
+      $(selector)[method]('list-menu-link-disabled').parent()[method]('list-menu-wrapper-disabled');
+    }
+    // Check/uncheck all functionality
+    function checkAll(base, checked) {
+      // Toggle all checkboxes on the table's body that exist on the first column.
+      base.find(listCheckboxesSelector).prop('checked', checked);
+      base.find('.list-row')[checked ? 'addClass' : 'removeClass']('list-row-selected');
+      toggleAction('#delete-selected', !checked);
+    }
+    function generalToggle() {
+      var checked = listCheckboxes.filter(':checked').length;
+      toggleAction('#delete-selected', checked === 0);
+      toggleAction('#deselect-all', checked === 0);
+      toggleAction('#select-all', checked === listCheckboxesLength);
+    }
+
+    var listCheckboxesSelector = '.list-selectable-checkbox', list = $('#list'), alertTimeout = 4000, listCheckboxes, listCheckboxesLength;
 
     // Automatically close alerts if there was any present.
     if ($('.alert').length > 0) {
       setTimeout(function() { $('.alert').alert('close'); }, alertTimeout);
     }
 
-    // Confirm before deleting one item
-    $('.delete-one').on('click', function(ev) {
-      ev.preventDefault();
-      $(this).addClass('active').siblings('.popover-delete-one')
-        .first().show().find('.cancel').on('click', function() {
+    // Only process list-related JavaScript if there's a list!
+    if (list.length > 0) {
+      listCheckboxes = list.find(listCheckboxesSelector);
+      listCheckboxesLength = listCheckboxes.length;
+      
+      // Confirm before deleting one item
+      $('.list-row-action-delete-one').on('click', function(ev) {
+        ev.preventDefault();
+        $(this).addClass('list-row-action-wrapper-link-active')
+          .siblings('.list-row-action-popover-delete-one').first().show()
+          .find('.cancel').on('click', function() {
 
-          $(this).parents('.popover').hide().siblings('.delete-one').removeClass('active');
-        });
-    });
-
-    function toggleAction(selector, disabled) {
-      var method = disabled ? 'addClass' : 'removeClass';
-      $(selector)[method]('disabled').parent()[method]('disabled');
-    }
-
-    // Select/deselect record on row's click
-    $('#list tbody tr td').on('click', function(ev) {
-      var checkbox;
-      ev.stopPropagation();
-      if (ev.currentTarget.tagName == "TD") { 
-        checkbox = $(this).parent().find('.selectable :checkbox');
-        checkbox.prop('checked', !checkbox.prop('checked'));
-      }
-    });
-    // Check/uncheck all functionality
-    function checkAll(table, checked) {
-      // Toggle all checkboxes on the table's body that exist on the first column.
-      table.find(allCheckboxSelector).prop('checked', checked);
-      toggleAction('#delete-selected', !checked);
-    }
-    $('#select-all').on('click', function(ev) {
-      ev.preventDefault();
-      // We assume we want to stay on the dropdown to delete all perhaps
-      ev.stopPropagation();
-      checkAll($(this).parents('table'), true);
-      toggleAction('#select-all', true);
-      toggleAction('#deselect-all', false);
-    });
-    $('#deselect-all').on('click', function(ev) {
-      ev.preventDefault();
-      checkAll($(this).parents('table'), false);
-      toggleAction('#deselect-all', true);
-      toggleAction('#select-all', false);
-    });
-    // Delete selected
-    $('#delete-selected').on('click', function(ev) {
-      ev.preventDefault();
-      ev.stopPropagation();
-      if ($(this).is('.disabled')) return;
-
-      $(this).parent().addClass('active').parent('.dropdown').addClass('open');
-      $(this).addClass('active').siblings('.popover-delete-selected').first().show().find('.cancel').on('click', function() {
-        // Hide the popover on cancel
-        $(this).parents('.popover').hide().siblings('#delete-selected').removeClass('active').parent().removeClass('active');
-        // and close the dropdown
-        $(this).parents('.dropdown').removeClass('open');
+            $(this).parents('.list-row-action-popover-delete-one').hide()
+              .siblings('.list-row-action-delete-one').removeClass('list-row-action-wrapper-link-active');
+          });
       });
 
-      $(this).siblings('.popover-delete-selected').find(':hidden[data-delete-many-ids=true]').
-        val($(this).parents('table').find(allCheckboxSelector + ':checked').map(function() { return $(this).val(); }).toArray().join(','));
-    });
+      // Select/deselect record on row's click
+      list.find('.list-row').on('click', function(ev) {
+        var checkbox, willBeChecked;
+        ev.stopPropagation();
 
-    // Catch checkboxes check/uncheck and enable/disable the delete selected functionality
-    $('#list ' + allCheckboxSelector).on('click', function(ev) {
-      var all = $('#list ' + allCheckboxSelector), checked = all.filter(':checked').length, all_length = all.length;
-      ev.stopPropagation();
+        if (ev.currentTarget.tagName == 'TR') { 
+          checkbox = $(this).find('.list-selectable-checkbox');
+          willBeChecked = !checkbox.prop('checked');
+          checkbox.prop('checked', willBeChecked);
+          $(this)[willBeChecked ? 'addClass' : 'removeClass']('list-row-selected');
+          generalToggle();
+        }
+      });
+      // Select all action 
+      $('#select-all').on('click', function(ev) {
+        ev.preventDefault();
+        ev.stopPropagation();
+        if ($(this).is('.list-menu-link-disabled')) return;
 
-      toggleAction('#delete-selected', checked === 0);
-      toggleAction('#deselect-all', checked === 0);
-      toggleAction('#select-all', checked === all_length);
-    });
+        // We assume we want to stay on the dropdown to delete all perhaps
+        ev.stopPropagation();
+        checkAll(list, true);
+        toggleAction('#select-all', true);
+        toggleAction('#deselect-all', false);
+      });
+      // Deselect all action 
+      $('#deselect-all').on('click', function(ev) {
+        ev.preventDefault();
+        if ($(this).is('.list-menu-link-disabled')) return;
+
+        checkAll(list, false);
+        toggleAction('#deselect-all', true);
+        toggleAction('#select-all', false);
+      });
+      // Delete selected
+      $('#delete-selected').on('click', function(ev) {
+        ev.preventDefault();
+        ev.stopPropagation();
+        if ($(this).is('.list-menu-link-disabled')) return;
+
+        // Open the popup to confirm deletion
+        $(this).parent().addClass('active').parent('.dropdown').addClass('open');
+        $(this).addClass('active')
+          .siblings('.list-menu-popover-delete-selected').first().show()
+          .find('.cancel').on('click', function() {
+          
+            // Hide the popover on cancel
+            $(this).parents('.list-menu-popover-delete-selected').hide()
+              .siblings('#delete-selected').removeClass('active').parent().removeClass('active');
+            // and close the dropdown
+            $(this).parents('.dropdown').removeClass('open');
+          });
+
+        $(this).siblings('.list-menu-popover-delete-selected').find(':hidden[data-delete-many-ids=true]').
+          val(listCheckboxes.filter(':checked').map(function() { return $(this).val(); }).toArray().join(','));
+      });
+
+      // Catch checkboxes check/uncheck and enable/disable the delete selected functionality
+      listCheckboxes.on('click', function(ev) {
+        ev.stopPropagation();
+
+        $(this).parent('.list-row')[$(this).is(':checked') ? 'addClass' : 'removeClass']('list-row-selected');
+
+        generalToggle();
+      });
+    }
 
     // Autofocus first field with an error. (usability)
     $('.has-error :input').first().focus();
