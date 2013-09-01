f8692462fc6ba3e4764b6369cab560817409dc8c
Matthias Günther <matthias.guenther@wikimatze.de>
2013-09-01 16:34:49 +0200

    Remove obsolete comments for the caller file

--- padrino-core/lib/padrino-core/caller.rb
+++ padrino-core/lib/padrino-core/caller.rb
@@ -2,23 +2,23 @@ module Padrino
 
   # List of callers in a Padrino application that should be ignored as part of a stack trace.
   PADRINO_IGNORE_CALLERS = [
-    %r{lib/padrino-.*$},                             # all padrino code
-    %r{/padrino-.*/(lib|bin)},                       # all padrino code
-    %r{/bin/padrino$},                               # all padrino code
-    %r{/sinatra(/(base|main|showexceptions))?\.rb$}, # all sinatra code
-    %r{lib/tilt.*\.rb$},                             # all tilt code
-    %r{lib/rack.*\.rb$},                             # all rack code
-    %r{lib/mongrel.*\.rb$},                          # all mongrel code
-    %r{lib/shotgun.*\.rb$},                          # all shotgun lib
-    %r{bin/shotgun$},                                # shotgun binary
-    %r{\(.*\)},                                      # generated code
-    %r{shoulda/context\.rb$},                        # shoulda hacks
-    %r{mocha/integration},                           # mocha hacks
-    %r{test/unit},                                   # test unit hacks
-    %r{rake_test_loader\.rb},                        # rake hacks
-    %r{custom_require\.rb$},                         # rubygems require hacks
-    %r{active_support},                              # active_support require hacks
-    %r{/thor}                                        # thor require hacks
+    %r{lib/padrino-.*$},
+    %r{/padrino-.*/(lib|bin)},
+    %r{/bin/padrino$},
+    %r{/sinatra(/(base|main|showexceptions))?\.rb$},
+    %r{lib/tilt.*\.rb$},
+    %r{lib/rack.*\.rb$},
+    %r{lib/mongrel.*\.rb$},
+    %r{lib/shotgun.*\.rb$},
+    %r{bin/shotgun$},
+    %r{\(.*\)},
+    %r{shoulda/context\.rb$},
+    %r{mocha/integration},
+    %r{test/unit},
+    %r{rake_test_loader\.rb},
+    %r{custom_require\.rb$},
+    %r{active_support},
+    %r{/thor}
   ] unless defined?(PADRINO_IGNORE_CALLERS)
 
   ##
@@ -50,4 +50,4 @@ module Padrino
         reject { |file,line| PADRINO_IGNORE_CALLERS.any? { |pattern| file =~ pattern } }.
         map    { |file,line| file }
     end
-end # Padrino
+end
