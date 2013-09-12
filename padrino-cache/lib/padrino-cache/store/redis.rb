782b2341a35927c0d4e68b5200b34aaf9da8346d
Matthias Günther <matthias.guenther@wikimatze.de>
2013-09-12 18:55:30 +0200

    Cleanup of redis file

--- padrino-cache/lib/padrino-cache/store/redis.rb
+++ padrino-cache/lib/padrino-cache/store/redis.rb
@@ -9,7 +9,7 @@ module Padrino
         # Initialize Redis store with client connection.
         #
         # @param client
-        #   Instance of Redis client
+        #   Instance of Redis client.
         #
         # @example
         #   Padrino.cache = Padrino::Cache::Store::Redis.new(::Redis.new(:host => '127.0.0.1', :port => 6379, :db => 0))
@@ -18,14 +18,13 @@ module Padrino
         #   # you can provide a marshal parser (to store ruby objects)
         #   set :cache, Padrino::Cache::Store::Redis.new(::Redis.new(:host => '127.0.0.1', :port => 6379, :db => 0), :parser => :marshal)
         #
-        # @api public
         def initialize(client, options={})
           @backend = client
           super(options)
         end
 
         ##
-        # Return the a value for the given key
+        # Return the value for the given key.
         #
         # @param [String] key
         #   cache key
@@ -34,7 +33,6 @@ module Padrino
         #   # with MyApp.cache.set('records', records)
         #   MyApp.cache.get('records')
         #
-        # @api public
         def get(key)
           code = @backend.get(key)
           return nil unless code
@@ -42,7 +40,7 @@ module Padrino
         end
 
         ##
-        # Set the value for a given key and optionally with an expire time
+        # Set the value for a given key and optionally with an expire time.
         # Default expiry is 86400.
         #
         # @param [String] key
@@ -54,7 +52,6 @@ module Padrino
         #   MyApp.cache.set('records', records)
         #   MyApp.cache.set('records', records, :expires_in => 30) # => 30 seconds
         #
-        # @api public
         def set(key, value, opts = nil)
           value = parser.encode(value)
           if opts && opts[:expires_in] && opts[:expires_in] >= 0
@@ -66,39 +63,35 @@ module Padrino
         end
 
         ##
-        # Delete the value for a given key
+        # Delete the value for a given key.
         #
         # @param [String] key
         #   cache key
         #
         # @example
-        #   # with: MyApp.cache.set('records', records)
         #   MyApp.cache.delete('records')
         #
-        # @api public
         def delete(key)
           @backend.del(key)
         end
 
         ##
-        # Reinitialize your cache
+        # Reinitialize your cache.
         #
         # @example
-        #   # with: MyApp.cache.set('records', records)
         #   MyApp.cache.flush
         #   MyApp.cache.get('records') # => nil
         #
-        # @api public
         def flush
           @backend.flushdb
         end
 
         ##
-        # Redis has a ton of powerful features (see: https://github.com/redis/redis-rb), which we
-        # can't use due to how strict the cache library is. This method catches all method calls and
-        # tries to pass them on the the redis gem.
+        # Redis has a ton of powerful features (see:
+        # https://github.com/redis/redis-rb), which we can't use due
+        # to how strict the cache library is. This method catches all method calls and
+        # tries to pass them on the redis gem.
         #
-        # @api private
         def method_missing(name, *args, &block)
           if @backend.respond_to?(name)
             @backend.send(name, *args, &block)
@@ -106,7 +99,7 @@ module Padrino
             super
           end
         end
-      end # Redis
-    end # Store
-  end # Cache
-end # Padrino
+      end
+    end
+  end
+end
