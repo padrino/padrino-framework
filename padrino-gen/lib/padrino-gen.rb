32b90e6d6c615b367e011b0a34b26afd23a431ba
Matthias Günther <matthias.guenther@wikimatze.de>
2013-09-12 19:12:22 +0200

    Cleanup of padrino-gen file

--- padrino-gen/lib/padrino-gen.rb
+++ padrino-gen/lib/padrino-gen.rb
@@ -5,7 +5,7 @@ require 'active_support/ordered_hash'
 
 module Padrino
   ##
-  # This module it's used for register generators
+  # This module it's used for register generators.
   #
   # Can be useful for 3rd party generators:
   #
@@ -14,7 +14,7 @@ module Padrino
   #     Padrino::Generators.add_generator(:custom_generator, self)
   #   end
   #
-  # Now for handle generators in padrino you need to add it to into +load_paths+
+  # Now for handle generators in padrino you need to add it to into +load_paths+.
   #
   # Padrino::Generators.load_paths << "custom_generator.rb"
   #
@@ -24,23 +24,21 @@ module Padrino
 
     class << self
       ##
-      # Here we store our generators paths
+      # Here we store our generators paths.
       #
-      # @api semipublic
       def load_paths
         @_files ||= []
       end
 
       ##
-      # Return an ordered list of task with their class
+      # Return an ordered list of task with their class.
       #
-      # @api semipublic
       def mappings
         @_mappings ||= ActiveSupport::OrderedHash.new
       end
 
       ##
-      # Global add a new generator class to +padrino-gen+
+      # Global add a new generator class to +padrino-gen+.
       #
       # @param [Symbol] name
       #   key name for generator mapping
@@ -52,7 +50,6 @@ module Padrino
       # @example
       #   Padrino::Generators.add_generator(:controller, Controller)
       #
-      # @api semipublic
       def add_generator(name, klass)
         mappings[name] = klass
       end
@@ -60,7 +57,6 @@ module Padrino
       ##
       # Load Global Actions and Component Actions then all files in +load_path+.
       #
-      # @api private
       def load_components!
         require 'padrino-gen/generators/actions'
         require 'padrino-gen/generators/components/actions'
@@ -68,15 +64,11 @@ module Padrino
         load_paths.flatten.each { |file| require file  }
       end
     end
-  end # Generators
-end # Padrino
+  end
+end
 
-##
-# We add our generators to Padrino::Generators
-#
+# We add our generators to Padrino::Generators.
 Padrino::Generators.load_paths << Dir[File.dirname(__FILE__) + '/padrino-gen/generators/{project,app,mailer,controller,model,migration,plugin,component}.rb']
 
-##
-# We add our tasks to padrino-core
-#
+# We add our tasks to padrino-core.
 Padrino::Tasks.files << Dir[File.dirname(__FILE__) + "/padrino-gen/padrino-tasks/**/*.rb"]
