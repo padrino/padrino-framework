964a79685302008b983f204d05d538a4c3920136
Matthias Günther <matthias.guenther@wikimatze.de>
2013-09-14 06:11:23 +0200

    Cleanup of command file

--- padrino-gen/lib/padrino-gen/command.rb
+++ padrino-gen/lib/padrino-gen/command.rb
@@ -3,17 +3,16 @@ require 'padrino-core/command'
 module Padrino
   ##
   # This method return the correct location of padrino-gen bin or
-  # exec it using Kernel#system with the given args
+  # exec it using Kernel#system with the given args.
   #
-  # @param [Array<String>] args
-  #   Splat of arguments to pass to padrino-gen
+  # @param [Array<String>] args.
+  #   Splat of arguments to pass to padrino-gen.
   #
   # @example
   #   Padrino.bin_gen(:app, name.to_s, "-r=#{destination_root}")
   #
-  # @api semipublic
   def self.bin_gen(*args)
     @_padrino_gen_bin ||= [Padrino.ruby_command, File.expand_path("../../../bin/padrino-gen", __FILE__)]
     system args.unshift(@_padrino_gen_bin).join(" ")
   end
-end # Padrino
+end
