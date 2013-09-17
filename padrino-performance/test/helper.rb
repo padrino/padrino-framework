1cc3c10610bdaaaa2974cc5689433057b552eb69
Matthias Günther <matthias.guenther@wikimatze.de>
2013-09-17 21:43:46 +0200

    Complete test for os module

--- padrino-performance/test/helper.rb
+++ padrino-performance/test/helper.rb
@@ -2,6 +2,7 @@ require File.expand_path('../../../load_paths', __FILE__)
 require File.join(File.dirname(__FILE__), '..', '..', 'padrino-core', 'test', 'mini_shoulda')
 
 require 'padrino-core'
+require 'padrino-performance'
 require 'rack'
 require 'rack/test'
 
