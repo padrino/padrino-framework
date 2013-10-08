28c4b07690d5608ac94cb071820ab407a6546ba9
Daniel Mendler <mail@daniel-mendler.de>
2013-10-09 00:07:18 +0200

    Correct fix for Slim #1431, #1434, #1435

--- padrino-core/lib/padrino-core/application/rendering/extensions/slim.rb
+++ padrino-core/lib/padrino-core/application/rendering/extensions/slim.rb
@@ -4,20 +4,13 @@ begin
   if defined? Padrino::Rendering
     Padrino::Rendering.engine_configurations[:slim] =
       {:generator => Temple::Generators::RailsOutputBuffer,
-      :buffer => "@_out_buf", :use_html_safe => true}
+      :buffer => "@_out_buf", :use_html_safe => true, :disable_capture => true}
 
     class Slim::Template
       include Padrino::Rendering::SafeTemplate
 
       def precompiled_preamble(locals)
-        result = locals.map do |k,v|
-          if k.to_s =~ /\A[a-z_][a-zA-Z_0-9]*\z/
-            "#{k} = locals[#{k.inspect}]"
-          else
-            raise "invalid locals key: #{k.inspect} (keys must be variable names)"
-          end
-        end.join("\n")
-        "#{result}; __in_erb_template = true;"
+        "__in_slim_template = true;#{super}"
       end
     end
   end
