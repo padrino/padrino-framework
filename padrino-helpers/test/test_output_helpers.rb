8ae11502f0e91aa22adc772b209bdacf40cc4a0b
Igor Bochkariov <ujifgc@gmail.com>
2013-10-09 22:42:02 +0400

    check output count, enable skipped tests

--- padrino-helpers/test/test_output_helpers.rb
+++ padrino-helpers/test/test_output_helpers.rb
@@ -49,20 +49,20 @@ describe "OutputHelpers" do
   context 'for #capture_html method' do
     should "work for erb templates" do
       visit '/erb/capture_concat'
-      assert_have_selector 'p span', :content => "Captured Line 1"
-      assert_have_selector 'p span', :content => "Captured Line 2"
+      assert_have_selector 'p span', :content => "Captured Line 1", :count => 1
+      assert_have_selector 'p span', :content => "Captured Line 2", :count => 1
     end
 
     should "work for haml templates" do
       visit '/haml/capture_concat'
-      assert_have_selector 'p span', :content => "Captured Line 1"
-      assert_have_selector 'p span', :content => "Captured Line 2"
+      assert_have_selector 'p span', :content => "Captured Line 1", :count => 1
+      assert_have_selector 'p span', :content => "Captured Line 2", :count => 1
     end
 
     should "work for slim templates" do
       visit '/slim/capture_concat'
-      assert_have_selector 'p span', :content => "Captured Line 1"
-      assert_have_selector 'p span', :content => "Captured Line 2"
+      assert_have_selector 'p span', :content => "Captured Line 1", :count => 1
+      assert_have_selector 'p span', :content => "Captured Line 2", :count => 1
     end
   end
 
@@ -79,30 +79,27 @@ describe "OutputHelpers" do
 
     should "work for slim templates" do
       visit '/slim/capture_concat'
-      # TODO Get Slim concat working
-      # assert_have_selector 'p', :content => "Concat Line 3", :count => 1
+      assert_have_selector 'p', :content => "Concat Line 3", :count => 1
     end
   end
 
   context 'for #block_is_template?' do
     should "work for erb templates" do
       visit '/erb/capture_concat'
-      assert_have_selector 'p', :content => "The erb block passed in is a template", :class => 'is_template'
-      # TODO Get ERB template detection working (fix block_is_erb? method)
-      # assert_have_no_selector 'p', :content => "The ruby block passed in is a template", :class => 'is_template'
+      assert_have_selector 'p', :content => "The erb block passed in is a template", :class => 'is_template', :count => 1
+      assert_have_no_selector 'p', :content => "The ruby block passed in is a template", :class => 'is_template', :count => 1
     end
 
     should "work for haml templates" do
       visit '/haml/capture_concat'
-      assert_have_selector 'p', :content => "The haml block passed in is a template", :class => 'is_template'
-      assert_have_no_selector 'p', :content => "The ruby block passed in is a template", :class => 'is_template'
+      assert_have_selector 'p', :content => "The haml block passed in is a template", :class => 'is_template', :count => 1
+      assert_have_no_selector 'p', :content => "The ruby block passed in is a template", :class => 'is_template', :count => 1
     end
 
     should "work for slim templates" do
       visit '/slim/capture_concat'
-      # TODO Get Slim template detection working
-      # assert_have_selector 'p', :content => "The slim block passed in is a template", :class => 'is_template'
-      assert_have_no_selector 'p', :content => "The ruby block passed in is a template", :class => 'is_template'
+      assert_have_selector 'p', :content => "The slim block passed in is a template", :class => 'is_template', :count => 1
+      assert_have_no_selector 'p', :content => "The ruby block passed in is a template", :class => 'is_template', :count => 1
     end
   end
 
