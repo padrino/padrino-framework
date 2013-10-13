6230cc67564afce42b9d03a83e77a04514aaf669
Daniel Mendler <mail@daniel-mendler.de>
2013-10-13 15:00:00 +0200

    remove deprecated cache functionality

--- padrino-cache/lib/padrino-cache/helpers/page.rb
+++ padrino-cache/lib/padrino-cache/helpers/page.rb
@@ -64,11 +64,6 @@ module Padrino
           @_last_expires       = time
         end
 
-        def expires_in(time)
-          warn 'expires_in has been deprecated in favour of expires'
-          expires(time)
-        end
-
         ##
         # This helper is used within a route or route to indicate the name in the cache.
         #
