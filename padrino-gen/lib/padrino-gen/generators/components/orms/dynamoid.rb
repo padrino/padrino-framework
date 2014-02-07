ed0bb7e7688f0f7119b5a6a9c3ba194631d81b6a
nof <sakape@gmail.com>
2014-02-07 21:44:10 +0900

    add information is for using fake_dynamo

--- padrino-gen/lib/padrino-gen/generators/components/orms/dynamoid.rb
+++ padrino-gen/lib/padrino-gen/generators/components/orms/dynamoid.rb
@@ -12,6 +12,25 @@ Dynamoid.configure do |config|
   config.write_capacity = 20 # Write capacity for your tables
 end
 
+# If you use mock in testing [for example in case of using fake_dynamo],
+# the way is as following:
+#
+#   - install fake_dynamo
+#     gem install fake_dynamo --version 0.1.3
+#   - run
+#     fake_dynamo --port 4567
+# 
+# And then setting for AWS.config is as following:
+# 
+#   AWS.config({
+#     :access_key_id => 'xxx', # everything is ok
+#     :secret_access_key => 'xxx', # everything is ok
+#     :dynamo_db_endpoint => 'localhost', # fake_dynamo runs hostname
+#     :dynamo_db_port => 4567, # fake_dynamo listens port
+#     :use_ssl => false # fake_dynamo don't speak ssl
+#   })
+#
+# Additional information on https://github.com/ananthakumaran/fake_dynamo
 DYNAMOID
 
 def setup_orm
