705ba1cae9604fd4542f97c25a0423949bc595f0
Nathan Esquenazi <nesquena@gmail.com>
2014-02-09 03:15:20 -0800

    Bumped version to 0.12.0

--- padrino-performance/lib/padrino-performance/version.rb
+++ padrino-performance/lib/padrino-performance/version.rb
@@ -1,7 +1,7 @@
 module Padrino
   module Performance
     # The version constant for the current version of Padrino.
-    VERSION = '0.12.0.rc3' unless defined?(Padrino::VERSION)
+    VERSION = '0.12.0' unless defined?(Padrino::VERSION)
 
     #
     # The current Padrino version.
