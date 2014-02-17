c9575c0ca7716a28cdaf1cd756dc69c409eeb453
Igor Bochkariov <ujifgc@gmail.com>
2014-02-17 15:45:00 +0400

    add an example of database.yml usage for datamapper, closes #899

--- padrino-gen/lib/padrino-gen/generators/components/orms/datamapper.rb
+++ padrino-gen/lib/padrino-gen/generators/components/orms/datamapper.rb
@@ -9,6 +9,27 @@ DM = (<<-DM) unless defined?(DM)
 # # A Sqlite3 connection
 # DataMapper.setup(:default, "sqlite3://" + Padrino.root('db', "development.db"))
 #
+# # Setup DataMapper using config/database.yml
+# DataMapper.setup(:default, YAML.load_file(Padrino.root('config/database.yml'))[RACK_ENV])
+#
+# config/database.yml file:
+#
+# ---
+# development: &defaults
+#   adapter: mysql
+#   database: example_development
+#   username: user
+#   password: Pa55w0rd
+#   host: 127.0.0.1
+#
+# test:
+#   <<: *defaults
+#   database: example_test
+#
+# production:
+#   <<: *defaults
+#   database: example_production
+#
 
 DataMapper.logger = logger
 DataMapper::Property::String.length(255)
