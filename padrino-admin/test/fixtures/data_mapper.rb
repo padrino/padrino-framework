f04e62dcb1e7826e810078f4826a99abd2cf450e
Igor Bochkariov <ujifgc@gmail.com>
2014-07-01 12:55:42 +0400

    strip test padrino-admin datamapper fixture

--- padrino-admin/test/fixtures/data_mapper.rb
+++ padrino-admin/test/fixtures/data_mapper.rb
@@ -26,56 +26,15 @@ class Account
   property :salt,             String
   property :role,             String
 
-  # Validations
-  validates_presence_of      :email, :role
-  validates_presence_of      :password,                          :if => :password_required
-  validates_presence_of      :password_confirmation,             :if => :password_required
-  validates_length_of        :password, :min => 4, :max => 40,   :if => :password_required
-  validates_confirmation_of  :password,                          :if => :password_required
-  validates_length_of        :email,    :min => 3, :max => 100
-  validates_uniqueness_of    :email,    :case_sensitive => false
-  validates_format_of        :email,    :with => :email_address
-  validates_format_of        :role,     :with => /[A-Za-z]/
-
   has n, :categories
   def self.admin;  first(:role => "admin");  end
   def self.editor; first(:role => "editor"); end
-
-  # Callbacks
-  before :save, :generate_password
-
-  ##
-  # This method it's for authentication purpose
-  #
-  def self.authenticate(email, password)
-    account = first(:conditions => { :email => email }) if email.present?
-    account && account.password_clean == password ? account : nil
-  end
 
   ##
   # This method is used from AuthenticationHelper
   #
   def self.find_by_id(id)
     get(id)
-  end
-
-  ##
-  # This method it's used for retrive the original password.
-  #
-  def password_clean
-    crypted_password.decrypt(salt)
-  end
-
-  private
-
-  def generate_password
-    return if password.blank?
-    self.salt = Digest::SHA1.hexdigest("--#{Time.now}--#{email}--") if new?
-    self.crypted_password = password.encrypt(self.salt)
-  end
-
-  def password_required
-    crypted_password.blank? || !password.blank?
   end
 end
 
