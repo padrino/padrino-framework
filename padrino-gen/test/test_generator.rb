01fb358972ffd3ac2e2ddd5ba1b3a7758ae250fe
namusyaka <namusyaka@gmail.com>
2014-08-25 21:07:34 +0900

    Add a test for the helper generator

--- padrino-gen/test/test_generator.rb
+++ padrino-gen/test/test_generator.rb
@@ -3,7 +3,7 @@ require File.expand_path(File.dirname(__FILE__) + '/helper')
 describe "Generator" do
   describe "the generator" do
     it 'should have default generators' do
-      %w{controller mailer migration model app plugin component task}.each do |gen|
+      %w{controller mailer migration model app plugin component task helper}.each do |gen|
         assert Padrino::Generators.mappings.has_key?(gen.to_sym)
         assert_equal "Padrino::Generators::#{gen.camelize}", Padrino::Generators.mappings[gen.to_sym].name
         assert Padrino::Generators.mappings[gen.to_sym].respond_to?(:start)
