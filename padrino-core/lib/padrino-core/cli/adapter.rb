9a7efdaf92a71bd5510869d46e1f2a43a5f18e6f
Igor Bochkariov <ujifgc@gmail.com>
2015-01-10 14:37:44 +0400

    return proper exit codes in CLI, fixes #1849

--- padrino-core/lib/padrino-core/cli/adapter.rb
+++ padrino-core/lib/padrino-core/cli/adapter.rb
@@ -12,10 +12,19 @@ module Padrino
           options.symbolize_keys!
           if File.exist?(options[:pid])
             pid = File.read(options[:pid]).to_i
-            print "=> Sending INT to process with pid #{pid} wait "
-            Process.kill(2, pid) rescue nil
+            puts "=> Sending INT to process with pid #{pid}"
+            begin
+              Process.kill(2, pid)
+            rescue Errno::ESRCH, RangeError => error
+              puts error.message
+              exit
+            rescue Errno::EPERM => error
+              puts error.message
+              abort
+            end
           else
             puts "=> #{options[:pid]} not found!"
+            abort
           end
         end
       end
