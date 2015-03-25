498a5dc0fcac7d3723104e8de958f620941f7c8b
namusyaka <namusyaka@gmail.com>
2015-03-25 13:55:16 +0900

    Remove rack dependency and temporary show_exceptions code
    Sinatra 1.4.6 has been released :tada:

--- padrino-core/lib/padrino-core/application/show_exceptions.rb
+++ padrino-core/lib/padrino-core/application/show_exceptions.rb
@@ -4,35 +4,6 @@ module Padrino
   #
   # @private
   class ShowExceptions < Sinatra::ShowExceptions
-    if Sinatra::VERSION <= '1.4.5'
-      def call(env)
-        @app.call(env)
-      rescue Exception => e
-        errors, env["rack.errors"] = env["rack.errors"], @@eats_errors
-
-        if prefers_plain_text?(env)
-          content_type = "text/plain"
-          exception_string = dump_exception(e)
-        else
-          content_type = "text/html"
-          exception_string = pretty(env, e)
-        end
-
-        env["rack.errors"] = errors
-
-        # Post 893a2c50 in rack/rack, the #pretty method above, implemented in
-        # Rack::ShowExceptions, returns a String instead of an array.
-        body = Array(exception_string)
-
-        [
-          500,
-         {"Content-Type" => content_type,
-          "Content-Length" => Rack::Utils.bytesize(body.join).to_s},
-         body
-        ]
-      end
-    end
-
     private
 
     def frame_class(frame)
