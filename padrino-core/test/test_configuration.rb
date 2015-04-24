d7b3e56e229cb9e833eda17251c422bac6716cc7
Igor Bochkariov <ujifgc@gmail.com>
2015-04-24 15:16:55 +0400

    fix ruby19

--- padrino-core/test/test_configuration.rb
+++ padrino-core/test/test_configuration.rb
@@ -3,14 +3,14 @@ require File.expand_path(File.dirname(__FILE__) + '/helper')
 describe "PadrinoConfiguration" do
   it 'should be able to store values' do
     Padrino.config.val1 = 12345
-    assert_equal 12345, Padrino.config['val1']
+    assert_equal 12345, Padrino.config.val1
   end
 
   it 'should be able to configure with block' do
     Padrino.configure do |config|
       config.val2 = 54321
     end
-    assert_equal 54321, Padrino.config['val2']
+    assert_equal 54321, Padrino.config.val2
   end
 
   it 'should be able to configure with block' do
