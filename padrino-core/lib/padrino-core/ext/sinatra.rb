be96d21ec1fe1fb5ffae38f17594d2957b2c8c31
Igor Bochkariov <ujifgc@gmail.com>
2016-01-28 11:07:56 +0400

    remove patch applied in Sinatra v1.4.5

--- padrino-core/lib/padrino-core/ext/sinatra.rb
+++ padrino-core/lib/padrino-core/ext/sinatra.rb
@@ -13,17 +13,3 @@ class Sinatra::Request
     route_obj && route_obj.action
   end
 end
-
-##
-# This patches Sinatra to accept UTF-8 urls on JRuby 1.7.6
-#
-if RUBY_ENGINE == 'jruby' && defined?(JRUBY_VERSION) && JRUBY_VERSION > '1.7.4'
-  class Sinatra::Base
-    class << self
-      alias_method :old_generate_method, :generate_method
-      def generate_method(method_name, &block)
-        old_generate_method(method_name.to_sym, &block)
-      end
-    end
-  end
-end
