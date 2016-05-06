c422379a75e330e576fcc7fec632ab42ecade4f1
Igor Bochkariov <ujifgc@gmail.com>
2016-05-06 10:33:43 +0400

    remove debugging artifact

--- padrino-core/lib/padrino-core/logger.rb
+++ padrino-core/lib/padrino-core/logger.rb
@@ -407,7 +407,6 @@ module Padrino
     # Flush the entire buffer to the log object.
     #
     def flush
-      puts @buffer.size if $l
       return unless @buffer.size > 0
       @@mutex.synchronize do
         @log.write(@buffer.join(''))
