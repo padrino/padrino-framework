545e0f40b0da26bcd7d624a11284167864ff1fe7
Igor Bochkariov <ujifgc@gmail.com>
2016-05-08 12:47:28 +0400

    add --api option for project generator, closes #1921 [ci skip]

--- padrino-gen/lib/padrino-gen/generators/project.rb
+++ padrino-gen/lib/padrino-gen/generators/project.rb
@@ -27,6 +27,7 @@ module Padrino
       class_option :dev,              :desc => 'Use padrino from a git checkout',                                                    :default => false,       :type => :boolean
       class_option :tiny,             :desc => 'Generate tiny app skeleton',                                       :aliases => '-i', :default => false,       :type => :boolean
       class_option :lean,             :desc => 'Generate lean project without apps',                               :aliases => '-l', :default => false,       :type => :boolean
+      class_option :api,              :desc => 'Generate minimal project for APIs',                                                  :default => false,       :type => :boolean
       class_option :template,         :desc => 'Generate project from template',                                   :aliases => '-p', :default => nil,         :type => :string
       class_option :gem,              :desc => 'Generate project as a gem',                                        :aliases => '-g', :default => false,       :type => :boolean
       class_option :migration_format, :desc => 'Filename format for migrations (number, timestamp)',                                 :default => 'number',    :type => :string
