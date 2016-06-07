463cea8aaef65dcd9669d2326e4e64a53f83dfce
Igor Bochkariov <ujifgc@gmail.com>
2016-06-07 09:08:50 +0400

    move a test

--- padrino-core/test/test_reloader_storage.rb
+++ padrino-core/test/test_reloader_storage.rb
@@ -1,19 +1,19 @@
 require File.expand_path(File.dirname(__FILE__) + '/helper')
 
-describe "ObjectSpace" do
+describe "Padrino::Reloader::Storage" do
   describe "#classes" do
     it 'should take an snapshot of the current loaded classes' do
-      snapshot = ObjectSpace.classes
+      snapshot = Padrino::Reloader::Storage.send(:object_classes)
       assert_equal snapshot.include?(Padrino::Logger), true
     end
 
     it 'should return a Set object' do
-      snapshot = ObjectSpace.classes
+      snapshot = Padrino::Reloader::Storage.send(:object_classes)
       assert_equal snapshot.kind_of?(Set), true
     end
 
     it 'should be able to process a the class name given a block' do
-      klasses = ObjectSpace.classes do |klass|
+      klasses = Padrino::Reloader::Storage.send(:object_classes) do |klass|
         next unless klass.respond_to?(:name) # fix JRuby < 1.7.22
         if klass.name =~ /^Padrino::/
           klass
@@ -29,14 +29,14 @@ describe "ObjectSpace" do
 
   describe "#new_classes" do
     before do
-      @snapshot = ObjectSpace.classes
+      @snapshot = Padrino::Reloader::Storage.send(:object_classes)
     end
 
     it 'should return list of new classes' do
       class OSTest; end
       module OSTestModule; class B; end; end
 
-      new_classes = ObjectSpace.new_classes(@snapshot)
+      new_classes = Padrino::Reloader::Storage.send(:new_classes, @snapshot)
 
       assert_equal new_classes.size, 2
       assert_equal new_classes.include?(OSTest), true
@@ -44,7 +44,7 @@ describe "ObjectSpace" do
     end
 
     it 'should return a Set object' do
-      new_classes = ObjectSpace.new_classes(@snapshot)
+      new_classes = Padrino::Reloader::Storage.send(:new_classes, @snapshot)
       assert_equal new_classes.kind_of?(Set), true
     end
   end
