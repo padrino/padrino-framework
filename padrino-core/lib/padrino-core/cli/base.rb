88412fd2657801af1737490cc4d87f0757c847a4
Igor Bochkariov <ujifgc@gmail.com>
2016-07-11 16:29:06 +0400

    allow Pry as Padrino console shell, closes #2056 [ci skip]
    To use `padrino console` with Pry add `gem "pry"` to your project Gemfile

--- padrino-core/lib/padrino-core/cli/base.rb
+++ padrino-core/lib/padrino-core/cli/base.rb
@@ -28,16 +28,20 @@ module Padrino
       def console(*args)
         prepare :console
         require File.expand_path("../../version", __FILE__)
-        ARGV.clear
-        require 'irb'
-        begin
-          require "irb/completion"
-        rescue LoadError
-        end
         require File.expand_path('config/boot.rb')
         puts "=> Loading #{Padrino.env} console (Padrino v.#{Padrino.version})"
         require File.expand_path('../console', __FILE__)
-        IRB.start
+        ARGV.clear
+        if defined? Pry
+          Pry.start
+        else
+          require 'irb'
+          begin
+            require "irb/completion"
+          rescue LoadError
+          end
+          IRB.start
+        end
       end
 
       desc "generate", "Executes the Padrino generator with given options (alternatively use 'gen' or 'g')."
