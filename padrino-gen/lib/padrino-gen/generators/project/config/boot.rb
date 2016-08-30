1d0d00540d4cea8d3b65d98cd0f44299daf22cda
Igor Bochkariov <ujifgc@gmail.com>
2016-08-30 10:19:23 +0400

    add $LOAD_PATH instruction [ci skip]

--- padrino-gen/lib/padrino-gen/generators/project/config/boot.rb
+++ padrino-gen/lib/padrino-gen/generators/project/config/boot.rb
@@ -12,6 +12,10 @@ Bundler.require(:default, RACK_ENV)
 # Padrino::Logger::Config[:development][:log_level]  = :devel
 # Padrino::Logger::Config[:development][:log_static] = true
 #
+# ## Configure Ruby to allow requiring features from your lib folder
+#
+# $LOAD_PATH.unshift Padrino.root('lib')
+#
 # ## Enable logging of source location
 #
 # Padrino::Logger::Config[:development][:source_location] = true
