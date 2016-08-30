4a68119f2a88ff4a800aa8bbe4a23ff5f0d53f0a
Igor Bochkariov <ujifgc@gmail.com>
2016-08-30 10:02:52 +0400

    Bumped version to 0.13.3.1

--- padrino-core/lib/padrino-core/version.rb
+++ padrino-core/lib/padrino-core/version.rb
@@ -6,7 +6,7 @@
 #
 module Padrino
   # The version constant for the current version of Padrino.
-  VERSION = '0.13.3' unless defined?(Padrino::VERSION)
+  VERSION = '0.13.3.1' unless defined?(Padrino::VERSION)
 
   #
   # The current Padrino version.
