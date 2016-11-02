365f23b5dcf69d72bfab7fb6dcbf4c82fe5d7945
Igor Bochkariov <ujifgc@gmail.com>
2016-11-02 11:53:04 +0400

    remove deprecations planned for 0.14

--- padrino-support/lib/padrino-support.rb
+++ padrino-support/lib/padrino-support.rb
@@ -2,7 +2,6 @@
 # This file loads certain extensions required by Padrino.
 #
 require 'padrino-support/core_ext/string/colorize'
-require 'padrino-support/core_ext/object_space'
 require 'padrino-support/file_set'
 require 'padrino-support/utils'
 
