f161a7484689c39b7399a446731df2b6e8c7ed37
Adam Daniels <adam@mediadrive.ca>
2016-12-07 02:03:16 -0500

    Switch to exe/ for application bins. (#2099)
    Using bin/ for our own executables is problematic when storing binstubs
    in the same folder.
    
    New acceptable practice per Bundler is to use the exe/ path for our own
    bin.
    
    Issue #2097

--- padrino-gen/test/test_project_generator.rb
+++ padrino-gen/test/test_project_generator.rb
@@ -20,7 +20,7 @@ describe "ProjectGenerator" do
       assert_match_in_file("Padrino.mount('SampleProject::App', :app_file => Padrino.root('app/app.rb')).to('/')", "#{@apptmp}/sample_project/config/apps.rb")
       assert_file_exists("#{@apptmp}/sample_project/config/boot.rb")
       assert_file_exists("#{@apptmp}/sample_project/Rakefile")
-      assert_file_exists("#{@apptmp}/sample_project/bin/sample_project")
+      assert_file_exists("#{@apptmp}/sample_project/exe/sample_project")
       assert_file_exists("#{@apptmp}/sample_project/public/favicon.ico")
       assert_dir_exists("#{@apptmp}/sample_project/public/images")
       assert_dir_exists("#{@apptmp}/sample_project/public/javascripts")
