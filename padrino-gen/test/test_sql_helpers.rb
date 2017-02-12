padrino/padrino-framework

commit ce7ab0a4c2a606dbeef4212db0e74dba67ba81f7

remove #blank? and #present? from core and helpers

padrino-gen/lib/padrino-gen/padrino-tasks/sql-helpers.rb
@@ -7,7 +7,7 @@ module Padrino
         case adapter
           when 'postgres'
             environment = {}
-            environment['PGPASSWORD'] = password unless password.blank?
+            environment['PGPASSWORD'] = password unless password.empty?
 
             arguments = []
             arguments << "--encoding=#{charset}" if charset
@@ -18,7 +18,7 @@ module Padrino
             Process.wait Process.spawn(environment, 'createdb', *arguments)
           when 'mysql', 'mysql2'
             environment = {}
-            environment['MYSQL_PWD'] = password unless password.blank?
+            environment['MYSQL_PWD'] = password unless password.empty?
 
             arguments = []
             arguments << "--user=#{user}" if user
@@ -41,7 +41,7 @@ module Padrino
         case adapter
           when 'postgres'
             environment = {}
-            environment['PGPASSWORD'] = password unless password.blank?
+            environment['PGPASSWORD'] = password unless password.empty?
 
             arguments = []
             arguments << "--host=#{host}" if host
@@ -51,7 +51,7 @@ module Padrino
             Process.wait Process.spawn(environment, 'dropdb', *arguments)
           when 'mysql', 'mysql2'
             environment = {}
-            environment['MYSQL_PWD'] = password unless password.blank?
+            environment['MYSQL_PWD'] = password unless password.empty?
 
             arguments = []
             arguments << "--user=#{user}" if user

padrino-admin/lib/padrino-admin.rb
@@ -1,4 +1,3 @@
-require 'active_support/core_ext/object/blank'
 require 'padrino-core'
 require 'padrino-helpers'
 

padrino-admin/lib/padrino-admin/access_control.rb
@@ -104,7 +104,7 @@ module Padrino
         #    = link_to 'Profile', url(:accounts, :edit, :id => current_account.id)
         #
         def allowed?(account=nil, path=nil)
-          path = "/" if path.blank?
+          path = "/" if path.nil? || path.empty?
           role = account.role.to_sym rescue nil
           authorizations = @authorizations.find_all { |auth| auth.roles.include?(:any) }
           allowed_paths  = authorizations.map(&:allowed).flatten.uniq

padrino-admin/lib/padrino-admin/generators/admin_app.rb
@@ -135,6 +135,8 @@ module Padrino
             require_dependencies('bcrypt')
           end
 
+          require_dependencies 'activesupport', :version => ">= 3.1"
+
           # A nicer select box.
           # TODO FIXME This doesn't make much sense in here. Review.
           # gsub_file destination_root("admin/views/#{@model_plural}/_form.#{ext}"), "f.text_field :role, :class => :text_field", "f.select :role, :options => access_control.roles"

padrino-admin/lib/padrino-admin/generators/templates/app.rb.tt
@@ -1,3 +1,5 @@
+require 'active_support/core_ext/object/blank'
+
 module <%= @app_name %>
   class <%= @admin_name %> < Padrino::Application
     register Padrino::Mailer

padrino-admin/lib/padrino-admin/helpers/authentication_helpers.rb
@@ -78,7 +78,7 @@ module Padrino
         private
 
         def access_denied
-          if login_page.present?
+          if login_page
             redirect url(login_page)
           else
             halt 401, "You don't have permission for this resource"

padrino-admin/test/test_locale.rb
@@ -6,20 +6,20 @@ describe "Filters" do
     it "should have a vaild #{name} locale for admin" do
       base = YAML.load_file(file)
       base = base[name]['padrino']['admin']
-      assert base.present?
-      assert base['save'].present?
-      assert base['cancel'].present?
-      assert base['list'].present?
-      assert base['edit'].present?
-      assert base['new'].present?
-      assert base['show'].present?
-      assert base['delete'].present?
-      assert base['confirm'].present?
-      assert base['created_at'].present?
-      assert base['all'].present?
-      assert base['profile'].present?
-      assert base['settings'].present?
-      assert base['logout'].present?
+      assert base
+      assert base['save']
+      assert base['cancel']
+      assert base['list']
+      assert base['edit']
+      assert base['new']
+      assert base['show']
+      assert base['delete']
+      assert base['confirm']
+      assert base['created_at']
+      assert base['all']
+      assert base['profile']
+      assert base['settings']
+      assert base['logout']
     end
   end
 end

padrino-core/lib/padrino-core.rb
@@ -68,10 +68,10 @@ module Padrino
     #   No applications were mounted.
     #
     def application
-      warn 'WARNING! No apps are mounted. Please, mount apps in `config/apps.rb`' unless Padrino.mounted_apps.present?
+      warn 'WARNING! No apps are mounted. Please, mount apps in `config/apps.rb`' if Padrino.mounted_apps.empty?
       router = Padrino::Router.new
       Padrino.mounted_apps.each { |app| app.map_onto(router) }
-      middleware.present? ? add_middleware(router) : router
+      middleware.empty? ? router : add_middleware(router)
     end
 
     ##

commit f3cb50cf6e074aa5d45a6f30015fc9f55e078838

Handle nil database passwords for create_db and drop_db.
Commit ce7ab0a4c2a606dbeef4212db0e74dba67ba81f7 introduced a bug where
if the database configuration didn't specify a password attempts to
create/drop the database would fail. The underlying cause was that prior
to the above the code used `blank?` to which would catch both nil values
and empty values. The new code uses `empty?` which fails if used on a
nil value.

This patch fixes that by checking for nil in addition to an empty string.

This can be reproduced by creating a brand new app, using postgresql or
mysql, and setting the database configuration to this:

  postgres://localhost/nilpassword_development

And then running `rake sq:create` which will result in this:

  $ rake sq:create
  => Creating database 'nilpassword_development'
  rake aborted!
  NoMethodError: undefined method `empty?' for nil:NilClass
  .../gems/padrino-gen-0.13.3.3/lib/padrino-gen/padrino-tasks/sql-helpers.rb:10:in `create_db'
  .../gems/padrino-gen-0.13.3.3/lib/padrino-gen/padrino-tasks/sequel.rb:52:in `block (2 levels) in <top (required)>'
  .../gems/rake-12.0.0/exe/rake:27:in `<top (required)>'
  .../bin/ruby_executable_hooks:15:in `eval'
  .../bin/ruby_executable_hooks:15:in `<main>'
  Tasks: TOP => sq:create
  (See full trace by running task with --trace)

padrino-gen/lib/padrino-gen/padrino-tasks/sql-helpers.rb
@@ -7,7 +7,7 @@ module Padrino
         case adapter
           when 'postgres'
             environment = {}
-            environment['PGPASSWORD'] = password unless password.empty?
+            environment['PGPASSWORD'] = password unless password.nil? || password.empty?
 
             arguments = []
             arguments << "--encoding=#{charset}" if charset
@@ -18,7 +18,7 @@ module Padrino
             Process.wait Process.spawn(environment, 'createdb', *arguments)
           when 'mysql', 'mysql2'
             environment = {}
-            environment['MYSQL_PWD'] = password unless password.empty?
+            environment['MYSQL_PWD'] = password unless password.nil? || password.empty?
 
             arguments = []
             arguments << "--user=#{user}" if user
@@ -41,7 +41,7 @@ module Padrino
         case adapter
           when 'postgres'
             environment = {}
-            environment['PGPASSWORD'] = password unless password.empty?
+            environment['PGPASSWORD'] = password unless password.nil? || password.empty?
 
             arguments = []
             arguments << "--host=#{host}" if host
@@ -51,7 +51,7 @@ module Padrino
             Process.wait Process.spawn(environment, 'dropdb', *arguments)
           when 'mysql', 'mysql2'
             environment = {}
-            environment['MYSQL_PWD'] = password unless password.empty?
+            environment['MYSQL_PWD'] = password unless password.nil? || password.empty?
 
             arguments = []
             arguments << "--user=#{user}" if user

padrino-gen/test/test_sql_helpers.rb
@@ -1,0 +1,105 @@
+require File.expand_path(File.dirname(__FILE__) + "/helper")
+require File.expand_path(File.dirname(__FILE__) + "/../lib/padrino-gen/padrino-tasks/sql-helpers")
+
+describe "SqlHelpers" do
+  def setup
+    Process.expects(:wait)
+  end
+
+  describe "create_db" do
+    describe "postgres" do
+      it "does not set PGPASSWORD when password is nil" do
+        Process.expects(:spawn).with() do |environment, *args|
+          environment == {}
+        end
+        Padrino::Generators::SqlHelpers.create_db("postgres", nil, nil, nil, "database", nil, nil)
+      end
+
+      it "does not set PGPASSWORD when password is blank" do
+        Process.expects(:spawn).with() do |environment, *args|
+          environment == {}
+        end
+        Padrino::Generators::SqlHelpers.create_db("postgres", nil, "", nil, "database", nil, nil)
+      end
+
+      it "sets PGPASSWORD when password is present" do
+        Process.expects(:spawn).with() do |environment, *args|
+          environment == {"PGPASSWORD" => "hunter2"}
+        end
+        Padrino::Generators::SqlHelpers.create_db("postgres", nil, "hunter2", nil, "database", nil, nil)
+      end
+    end
+
+    describe "mysql" do
+      it "does not set MYSQL_PWD when password is nil" do
+        Process.expects(:spawn).with() do |environment, *args|
+          environment == {}
+        end
+        Padrino::Generators::SqlHelpers.create_db("mysql", nil, nil, nil, "database", nil, nil)
+      end
+
+      it "does not set MYSQL_PWD when password is blank" do
+        Process.expects(:spawn).with() do |environment, *args|
+          environment == {}
+        end
+        Padrino::Generators::SqlHelpers.create_db("mysql", nil, "", nil, "database", nil, nil)
+      end
+
+      it "sets MYSQL_PWD when password is present" do
+        Process.expects(:spawn).with() do |environment, *args|
+          environment == {"MYSQL_PWD" => "hunter2"}
+        end
+        Padrino::Generators::SqlHelpers.create_db("mysql", nil, "hunter2", nil, "database", nil, nil)
+      end
+    end
+  end
+
+  describe "drop_db" do
+    describe "postgres" do
+      it "does not set PGPASSWORD when password is nil" do
+        Process.expects(:spawn).with() do |environment, *args|
+          environment == {}
+        end
+        Padrino::Generators::SqlHelpers.drop_db("postgres", nil, nil, nil, "database")
+      end
+
+      it "does not set PGPASSWORD when password is blank" do
+        Process.expects(:spawn).with() do |environment, *args|
+          environment == {}
+        end
+        Padrino::Generators::SqlHelpers.drop_db("postgres", nil, "", nil, "database")
+      end
+
+      it "sets PGPASSWORD when password is present" do
+        Process.expects(:spawn).with() do |environment, *args|
+          environment == {"PGPASSWORD" => "hunter2"}
+        end
+        Padrino::Generators::SqlHelpers.drop_db("postgres", nil, "hunter2", nil, "database")
+      end
+    end
+
+    describe "mysql" do
+      it "does not set MYSQL_PWD when password is nil" do
+        Process.expects(:spawn).with() do |environment, *args|
+          environment == {}
+        end
+        Padrino::Generators::SqlHelpers.drop_db("mysql", nil, nil, nil, "database")
+      end
+
+      it "does not set MYSQL_PWD when password is blank" do
+        Process.expects(:spawn).with() do |environment, *args|
+          environment == {}
+        end
+        Padrino::Generators::SqlHelpers.drop_db("mysql", nil, "", nil, "database")
+      end
+
+      it "sets MYSQL_PWD when password is present" do
+        Process.expects(:spawn).with() do |environment, *args|
+          environment == {"MYSQL_PWD" => "hunter2"}
+        end
+        Padrino::Generators::SqlHelpers.drop_db("mysql", nil, "hunter2", nil, "database")
+      end
+    end
+  end
+end
+