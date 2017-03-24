06cb7a779ba8899723177b900eeb67c76d04c844
Igor Bochkariov <ujifgc@gmail.com>
2017-03-24 08:19:21 +0400

    use hamlit if available in Gemfile

--- padrino-helpers/test/test_rendering_extensions.rb
+++ padrino-helpers/test/test_rendering_extensions.rb
@@ -3,6 +3,8 @@ require File.expand_path(File.dirname(__FILE__) + '/helper')
 describe "Rendering Extensions" do
   describe 'for haml' do
     it 'should render haml_tag correctly' do
+      skip unless Padrino::Helpers::OutputHelpers.handlers[:haml].to_s == "Padrino::Helpers::OutputHelpers::HamlHandler"
+
       mock_app do
         get('/') { render :haml, '-haml_tag :div'}
       end
