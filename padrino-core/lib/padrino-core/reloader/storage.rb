338111d4de0e9b0b6f507f029e7e56405789fdec
Igor Bochkariov <ujifgc@gmail.com>
2018-11-06 11:24:24 +0400

    do exactly what test from #2193 suggests

--- padrino-core/lib/padrino-core/reloader/storage.rb
+++ padrino-core/lib/padrino-core/reloader/storage.rb
@@ -40,8 +40,13 @@ module Padrino
       end
 
       def rollback(name)
-        new_constants = new_classes(@old_entries[name][:constants])
-        new_constants.each{ |klass| Reloader.remove_constant(klass) }
+        new_classes(@old_entries[name][:constants]).each do |klass|
+          loaded_in_name = files.each do |file, data|
+                             next if file == name
+                             break if data[:constants].include?(klass)
+                           end
+          Reloader.remove_constant(klass) if loaded_in_name
+        end
         @old_entries.delete(name)
       end
 
