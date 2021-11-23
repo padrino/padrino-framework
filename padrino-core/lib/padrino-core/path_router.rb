dcdf018c2dde3705c32466d4bdb14c7bb1ad9231
Takeshi Yashiro <takeshi.yashiro@iniad.org>
2021-11-23 19:35:24 +0900

    Prioritize route with fewer names

--- padrino-core/lib/padrino-core/path_router.rb
+++ padrino-core/lib/padrino-core/path_router.rb
@@ -60,7 +60,8 @@ module Padrino
         fail InvalidRouteException if candidates.empty?
         i = 0
         route = candidates.sort_by! { |candidate|
-          [(params.keys.map(&:to_s) - candidate.matcher.names).length, i += 1] }.shift
+          # Tries to find the route that matches more, but with fewer names, in stable order
+          [(params.keys.map(&:to_s) - candidate.matcher.names).length, -candidate.matcher.names.size, i += 1] }.shift
         matcher = route.matcher
         params_for_expand = params.dup
         if !args.empty? && matcher.mustermann?
