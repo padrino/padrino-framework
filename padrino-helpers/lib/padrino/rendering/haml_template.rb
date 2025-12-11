07f23be8db7eaed79a968e0a274f5b001cf880b5
Carlos Silva <me@carlosfsilva.com>
2025-12-11 18:02:37 -0300

    Change HAML setup to support newer versions

--- padrino-helpers/lib/padrino/rendering/haml_template.rb
+++ padrino-helpers/lib/padrino/rendering/haml_template.rb
@@ -1,14 +1,18 @@
-module Haml
-  module Helpers
-    include XssMods
-    include ActionViewExtensions
-  end
+begin
+  using_modern_haml = false
+  require 'haml/helpers/xss_mods'
+  require 'haml/helpers/action_view_extensions'
 
-  module Util
-    def self.rails_xss_safe?
+  module Haml
+    Helpers.include(Helpers::XssMods)
+    Helpers.include(Helpers::ActionViewExtensions)
+
+    def Util.rails_xss_safe?
       true
     end
   end
+rescue LoadError
+  using_modern_haml = true
 end
 
 module Padrino
@@ -21,6 +25,13 @@ end
 
 Tilt.prefer(Padrino::Rendering::HamlTemplate, :haml)
 
-Padrino::Rendering.engine_configurations[:haml] = {
-  escape_html: true
-}
+Padrino::Rendering.engine_configurations[:haml] =
+  if using_modern_haml
+    {
+      buffer_class: 'SafeBuffer.new',
+      use_html_safe: true,
+      disable_capture: true
+    }
+  else
+    { escape_html: true }
+  end
